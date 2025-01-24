611491fc6a8998f0e30737249b98db1e5b8c5b87
centurystone619 <michellefoust6@gmail.com>
2025-01-24 15:55:10 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,5 +1,139 @@
 export const eventData=[
     [
+        {
+          artiste: "Tennessee Volunteers Mens Basketball vs. Florida Gators Men's Basketball",
+          img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
+          sec: "230",
+          row: "1",
+          seat: "12",
+          date: "Sat, Feb 01",
+          time: "12:00pm",
+          state: "Tennessee",
+          venue: "Food City Center",
+          type: "Free transfer",
+          eventData: {
+            month: "Feb",
+            day: "01",
+            year: "",
+            time: "Sat - 12:00",
+            region: "",
+            orderNo: ""
+          },
+          transfers: {
+            sec: "",
+            row: "",
+            seats: ['12','13','14','15'],
+            desc: "200 LEVEL",
+            GA: "",
+            color: "",
+            "venueMap": "",
+            "gate": "",
+            "btnColor": "",
+            sell: "y",
+            trnsfer: "y"
+          }
+        },
+        {
+          artiste: "Tennessee Volunteers Mens Basketball vs. Florida Gators Men's Basketball",
+          img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
+          sec: "230",
+          row: "1",
+          seat: "13",
+          date: "Sat, Feb 01",
+          time: "12:00pm",
+          state: "Tennessee",
+          venue: "Food City Center",
+          type: "Free transfer",
+          eventData: {
+            month: "Feb",
+            day: "01",
+            year: "",
+            time: "Sat - 12:00",
+            region: "",
+            orderNo: ""
+          },
+          transfers: {
+            sec: "",
+            row: "",
+            seats: ['13'],
+            desc: "200 LEVEL",
+            GA: "",
+            color: "",
+            venueMap: "",
+            gate: "",
+            btnColor: "",
+            sell: "",
+            trnsfer: ""
+          }
+        }, 
+        {
+          artiste: "Tennessee Volunteers Mens Basketball vs. Florida Gators Men's Basketball",
+          img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
+          sec: "230",
+          row: "1",
+          seat: "14",
+          date: "Sat, Feb 01",
+          time: "12:00pm",
+          state: "Tennessee",
+          venue: "Food City Center",
+          type: "Free transfer",
+          eventData: {
+            month: "Feb",
+            day: "01",
+            year: "",
+            time: "Sat - 12:00",
+            region: "",
+            orderNo: ""
+          },
+          transfers: {
+            sec: "",
+            row: "",
+            seats: ['14'],
+            desc: "200 LEVEL",
+            GA: "",
+            color: "",
+            "venueMap": "",
+            "gate": "",
+            "btnColor": "",
+            sell: "y",
+            trnsfer: "y"
+          }
+        },
+        {
+          artiste: "Tennessee Volunteers Mens Basketball vs. Florida Gators Men's Basketball",
+          img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
+          sec: "230",
+          row: "1",
+          seat: "15",
+          date: "Sat, Feb 01",
+          time: "12:00pm",
+          state: "Tennessee",
+          venue: "Food City Center",
+          type: "Free transfer",
+          eventData: {
+            month: "Feb",
+            day: "01",
+            year: "",
+            time: "Sat - 12:00",
+            region: "",
+            orderNo: ""
+          },
+          transfers: {
+            sec: "",
+            row: "",
+            seats: ['15'],
+            desc: "200 LEVEL",
+            GA: "",
+            color: "",
+            "venueMap": "",
+            "gate": "",
+            "btnColor": "",
+            sell: "y",
+            trnsfer: "y"
+          }
+        }
+      ],
+    [
       {
         artiste: "Tennessee Volunteers Mens Basketball vs. Vanderbilt Commodores Mens Basketball",
         img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
@@ -134,140 +268,7 @@ export const eventData=[
       }
     ],
 
-    [
-        {
-          artiste: "Tennessee Volunteers Mens Basketball vs. Florida Gators Men's Basketball",
-          img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
-          sec: "230",
-          row: "1",
-          seat: "12",
-          date: "Sat, Feb 01",
-          time: "12:00pm",
-          state: "Tennessee",
-          venue: "Food City Center",
-          type: "Free transfer",
-          eventData: {
-            month: "Feb",
-            day: "01",
-            year: "",
-            time: "Sat - 12:00",
-            region: "",
-            orderNo: ""
-          },
-          transfers: {
-            sec: "",
-            row: "",
-            seats: ['12','13','14','15'],
-            desc: "200 LEVEL",
-            GA: "",
-            color: "",
-            "venueMap": "",
-            "gate": "",
-            "btnColor": "",
-            sell: "y",
-            trnsfer: "y"
-          }
-        },
-        {
-          artiste: "Tennessee Volunteers Mens Basketball vs. Florida Gators Men's Basketball",
-          img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
-          sec: "230",
-          row: "1",
-          seat: "13",
-          date: "Sat, Feb 01",
-          time: "12:00pm",
-          state: "Tennessee",
-          venue: "Food City Center",
-          type: "Free transfer",
-          eventData: {
-            month: "Feb",
-            day: "01",
-            year: "",
-            time: "Sat - 12:00",
-            region: "",
-            orderNo: ""
-          },
-          transfers: {
-            sec: "",
-            row: "",
-            seats: ['13'],
-            desc: "200 LEVEL",
-            GA: "",
-            color: "",
-            venueMap: "",
-            gate: "",
-            btnColor: "",
-            sell: "",
-            trnsfer: ""
-          }
-        }, 
-        {
-          artiste: "Tennessee Volunteers Mens Basketball vs. Florida Gators Men's Basketball",
-          img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
-          sec: "230",
-          row: "1",
-          seat: "14",
-          date: "Sat, Feb 01",
-          time: "12:00pm",
-          state: "Tennessee",
-          venue: "Food City Center",
-          type: "Free transfer",
-          eventData: {
-            month: "Feb",
-            day: "01",
-            year: "",
-            time: "Sat - 12:00",
-            region: "",
-            orderNo: ""
-          },
-          transfers: {
-            sec: "",
-            row: "",
-            seats: ['14'],
-            desc: "200 LEVEL",
-            GA: "",
-            color: "",
-            "venueMap": "",
-            "gate": "",
-            "btnColor": "",
-            sell: "y",
-            trnsfer: "y"
-          }
-        },
-        {
-          artiste: "Tennessee Volunteers Mens Basketball vs. Florida Gators Men's Basketball",
-          img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
-          sec: "230",
-          row: "1",
-          seat: "15",
-          date: "Sat, Feb 01",
-          time: "12:00pm",
-          state: "Tennessee",
-          venue: "Food City Center",
-          type: "Free transfer",
-          eventData: {
-            month: "Feb",
-            day: "01",
-            year: "",
-            time: "Sat - 12:00",
-            region: "",
-            orderNo: ""
-          },
-          transfers: {
-            sec: "",
-            row: "",
-            seats: ['15'],
-            desc: "200 LEVEL",
-            GA: "",
-            color: "",
-            "venueMap": "",
-            "gate": "",
-            "btnColor": "",
-            sell: "y",
-            trnsfer: "y"
-          }
-        }
-      ],
+   
 
 
 ]
