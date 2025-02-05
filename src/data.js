f479e0b764c36159171b8b79aa43729af1fc5886
centurystone619 <michellefoust6@gmail.com>
2025-02-05 17:33:14 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,138 +1,138 @@
 export const eventData=[
-  [
-    {
-      artiste: "Treaty Oak Revival",
-      img: "https://media.ticketmaster.com/en-us/dam/a/21d/cebf074e-44a7-4c2f-9e58-6d3f839a821d_1846981_CUSTOM.jpg",
-      sec: "GA10",
-      row: "GA3",
-      seat: "-",
-      date: "Fri, Jan 31",
-      time: "8pm",
-      state: "Minnesota",
-      venue: "Armory",
-      type: "Standard Admission",
-      eventData: {
-        month: "Jan",
-        day: "31",
-        year: "",
-        time: "Fri - 8",
-        region: "",
-        orderNo: ""
+    [
+      {
+        artiste: "Clemson University Tigers Mens Basketball vs. Duke Blue Devils Men's Basketball",
+        img: "https://media.ticketmaster.com/en-us/dam/a/3c2/35cebaaa-9bf3-452a-9fdf-17ddc23cf3c2_CUSTOM.jpg",
+        sec: "104",
+        row: "Q",
+        seat: "8",
+        date: "Sat, Feb 8",
+        time: "6pm",
+        state: "South Carolina",
+        venue: "Littlejohn Coliseum",
+        type: "Resale Ticket",
+        eventData: {
+          month: "Feb",
+          day: "8",
+          year: "",
+          time: "Sat - 8",
+          region: "",
+          orderNo: ""
+        },
+        transfers: {
+          sec: "",
+          row: "",
+          seats: ['8','9','10','11'],
+            ticketId:['','','',''],
+          desc: "LOWER LEVEL",
+          GA: "",
+          color: "",
+          "venueMap": "https://www.ticketmaster.com/littlejohn-coliseum-tickets-clemson/venue/368918",
+          "gate": "",
+          "btnColor": "",
+          sell: "y",
+          trnsfer: "y"
+        }
       },
-      transfers: {
-        sec: "",
-        row: "",
-        seats: ['','','',''],
-          ticketId:['1','2','3','4'],
-        desc: "GA FLR STANDING",
-        GA: "",
-        color: "",
-        "venueMap": "Armory",
-        "gate": "",
-        "btnColor": "",
-        sell: "y",
-        trnsfer: "y"
-      }
-    },
-    {
-      artiste: "Treaty Oak Revival",
-      img: "https://media.ticketmaster.com/en-us/dam/a/21d/cebf074e-44a7-4c2f-9e58-6d3f839a821d_1846981_CUSTOM.jpg",
-      sec: "GA10",
-      row: "GA3",
-      seat: "-",
-      date: "Fri, Jan 31",
-      time: "8pm",
-      state: "Minnesota",
-      venue: "Armory",
-      type: "Standard Admission",
-      eventData: {
-        month: "Jan",
-        day: "31",
-        year: "2025",
-        time: "Fri - 8",
-        region: "",
-        orderNo: ""
+      {
+        artiste: "Clemson University Tigers Mens Basketball vs. Duke Blue Devils Men's Basketball",
+        img: "https://media.ticketmaster.com/en-us/dam/a/3c2/35cebaaa-9bf3-452a-9fdf-17ddc23cf3c2_CUSTOM.jpg",
+        sec: "104",
+        row: "Q",
+        seat: "9",
+        date: "Sat, Feb 8",
+        time: "6pm",
+        state: "South Carolina",
+        venue: "Littlejohn Coliseum",
+        type: "Resale Ticket",
+        eventData: {
+          month: "Feb",
+          day: "8",
+          year: "2025",
+          time: "Sat - 8",
+          region: "",
+          orderNo: ""
+        },
+        transfers: {
+          sec: "",
+          row: "",
+          seats: ['8','9','10','11'],
+          desc: "LOWER LEVEL",
+          GA: "",
+          color: "",
+          venueMap: "https://www.ticketmaster.com/littlejohn-coliseum-tickets-clemson/venue/368918",
+          gate: "",
+          btnColor: "",
+          sell: "",
+          trnsfer: ""
+        }
+      }, 
+      {
+        artiste: "Tyler, The Creator - CHROMAKOPIA: THE WORLD TOUR",
+        img: "https://s1.ticketm.net/dam/a/3a3/04ee9987-d0dd-41f6-a169-afc7727293a3_EVENT_DETAIL_PAGE_16_9.jpg",
+        sec: "118",
+        row: "6",
+        seat: "12",
+        date: "Tue, Feb 04",
+        time: "7:30pm",
+        state: "Minnesota",
+        venue: "Xcel Energy Center, Saint Paul",
+        type: "Tyler- The Creator Presale",
+        eventData: {
+          month: "Feb",
+          day: "04",
+          year: "2025",
+          time: "Tue - 19:30",
+          region: "",
+          orderNo: ""
+        },
+        
+        transfers: {
+          sec: "",
+          row: "",
+          seats: ['10','11','12','13'],
+          desc: "LOWER LEVEL",
+          GA: "",
+          color: "",
+          "venueMap": "",
+          "gate": "",
+          "btnColor": "",
+          sell: "y",
+          trnsfer: "y"
+        }
       },
-      transfers: {
-        sec: "",
-        row: "",
-        seats: [],
-        desc: "GA FLR STANDING",
-        GA: "",
-        color: "",
-        venueMap: "",
-        gate: "",
-        btnColor: "",
-        sell: "",
-        trnsfer: ""
+      {
+        artiste: "Tyler, The Creator - CHROMAKOPIA: THE WORLD TOUR",
+        img: "https://s1.ticketm.net/dam/a/3a3/04ee9987-d0dd-41f6-a169-afc7727293a3_EVENT_DETAIL_PAGE_16_9.jpg",
+        sec: "118",
+        row: "6",
+        seat: "13",
+        date: "Tue, Feb 04",
+        time: "7:30pm",
+        state: "Minnesota",
+        venue: "Xcel Energy Center, Saint Paul",
+        type: "Tyler- The Creator Presale",
+        eventData: {
+          month: "Feb",
+          day: "04",
+          year: "2025",
+          time: "Tue - 19:30",
+          region: "",
+          orderNo: ""
+        },
+     transfers: {
+          sec: "",
+          row: "",
+          seats: ['10','11','12','13'],
+          desc: "LOWER LEVEL",
+          GA: "",
+          color: "",
+          "venueMap": "",
+          "gate": "",
+          "btnColor": "",
+          sell: "y",
+          trnsfer: "y"
+        }
       }
-    }, 
-    {
-      artiste: "Tyler, The Creator - CHROMAKOPIA: THE WORLD TOUR",
-      img: "https://s1.ticketm.net/dam/a/3a3/04ee9987-d0dd-41f6-a169-afc7727293a3_EVENT_DETAIL_PAGE_16_9.jpg",
-      sec: "118",
-      row: "6",
-      seat: "12",
-      date: "Tue, Feb 04",
-      time: "7:30pm",
-      state: "Minnesota",
-      venue: "Xcel Energy Center, Saint Paul",
-      type: "Tyler- The Creator Presale",
-      eventData: {
-        month: "Feb",
-        day: "04",
-        year: "2025",
-        time: "Tue - 19:30",
-        region: "",
-        orderNo: ""
-      },
-      
-      transfers: {
-        sec: "",
-        row: "",
-        seats: ['10','11','12','13'],
-        desc: "LOWER LEVEL",
-        GA: "",
-        color: "",
-        "venueMap": "",
-        "gate": "",
-        "btnColor": "",
-        sell: "y",
-        trnsfer: "y"
-      }
-    },
-    {
-      artiste: "Tyler, The Creator - CHROMAKOPIA: THE WORLD TOUR",
-      img: "https://s1.ticketm.net/dam/a/3a3/04ee9987-d0dd-41f6-a169-afc7727293a3_EVENT_DETAIL_PAGE_16_9.jpg",
-      sec: "118",
-      row: "6",
-      seat: "13",
-      date: "Tue, Feb 04",
-      time: "7:30pm",
-      state: "Minnesota",
-      venue: "Xcel Energy Center, Saint Paul",
-      type: "Tyler- The Creator Presale",
-      eventData: {
-        month: "Feb",
-        day: "04",
-        year: "2025",
-        time: "Tue - 19:30",
-        region: "",
-        orderNo: ""
-      },
-   transfers: {
-        sec: "",
-        row: "",
-        seats: ['10','11','12','13'],
-        desc: "LOWER LEVEL",
-        GA: "",
-        color: "",
-        "venueMap": "",
-        "gate": "",
-        "btnColor": "",
-        sell: "y",
-        trnsfer: "y"
-      }
-    }
-  ]
-  ]
+    ]
+    ]
