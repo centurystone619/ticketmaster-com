8ecaa7910b1a8c9b3fbf5ffa03e840a4c3f742b8
centurystone619 <michellefoust6@gmail.com>
2025-02-12 14:20:40 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,21 +1,156 @@
 export const eventData=[
-    [
+  [
+    {
+      artiste: "Tennessee Volunteers Mens Basketball vs. Vanderbilt Commodores Mens Basketball",
+      img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
+      sec: "213",
+      row: "2",
+      seat: "16",
+      date: "Sat, Feb 15",
+      time: "1pm",
+      state: "Tennessee",
+      venue: "Food City Center",
+      type: "",
+      eventData: {
+        month: "Feb",
+        day: "15",
+        year: "",
+        time: "Sat - 1",
+        region: "",
+        orderNo: ""
+      },
+      transfers: {
+        sec: "",
+        row: "",
+        seats: ['16','17','18','19'],
+        desc: "200 LEVEL",
+        GA: "",
+        color: "Orange",
+        "venueMap": "",
+        "gate": "",
+        "btnColor": "",
+        sell: "y",
+        trnsfer: "y"
+      }
+    },
+    {
+      artiste: "Tennessee Volunteers Mens Basketball vs. Vanderbilt Commodores Mens Basketball",
+      img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
+      sec: "213",
+      row: "2",
+      seat: "17",
+      date: "Sat, Feb 15",
+      time: "1pm",
+      state: "Tennessee",
+      venue: "Food City Center",
+      type: "",
+      eventData: {
+        month: "Feb",
+        day: "15",
+        year: "",
+        time: "Sat - 1",
+        region: "",
+        orderNo: ""
+      },
+      transfers: {
+        sec: "",
+        row: "",
+        seats: ['17'],
+        desc: "200 LEVEL",
+        GA: "",
+        color: "Orange",
+        venueMap: "",
+        gate: "",
+        btnColor: "",
+        sell: "",
+        trnsfer: ""
+      }
+    }, 
+    {
+      artiste: "Tennessee Volunteers Mens Basketball vs. Vanderbilt Commodores Mens Basketball",
+      img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
+      sec: "230",
+      row: "1",
+      seat: "14",
+      date: "Sat, Feb 15",
+      time: "1:00pm",
+      state: "Tennessee",
+      venue: "Food City Center",
+      type: "Free transfer",
+      eventData: {
+        month: "Feb",
+        day: "15",
+        year: "",
+        time: "Sat - 1:00",
+        region: "",
+        orderNo: ""
+      },
+      transfers: {
+        sec: "",
+        row: "",
+        seats: ['14'],
+        desc: "200 LEVEL",
+        GA: "",
+        color: "",
+        "venueMap": "",
+        "gate": "",
+        "btnColor": "",
+        sell: "y",
+        trnsfer: "y"
+      }
+    },
+    {
+      artiste: "Tennessee Volunteers Mens Basketball vs. Vanderbilt Commodores Mens Basketball",
+      img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
+      sec: "230",
+      row: "1",
+      seat: "15",
+      date: "Sat, Feb 15",
+      time: "1:00pm",
+      state: "Tennessee",
+      venue: "Food City Center",
+      type: "Free transfer",
+      eventData: {
+        month: "Feb",
+        day: "15",
+        year: "",
+        time: "Sat - 1:00",
+        region: "",
+        orderNo: ""
+      },
+      transfers: {
+        sec: "",
+        row: "",
+        seats: ['15'],
+        desc: "200 LEVEL",
+        GA: "",
+        color: "",
+        "venueMap": "",
+        "gate": "",
+        "btnColor": "",
+        sell: "y",
+        trnsfer: "y"
+      }
+    }
+  ],
+
+  [
       {
-        artiste: "Tennessee Volunteers Mens Basketball vs. Vanderbilt Commodores Mens Basketball",
+        artiste: "Tennessee Volunteers Mens Basketball vs. Florida Gators Men's Basketball",
         img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
         sec: "230",
         row: "1",
         seat: "12",
-        date: "Sat, Feb 15",
-        time: "1:00pm",
+        date: "Sat, Feb 01",
+        time: "12:00pm",
         state: "Tennessee",
         venue: "Food City Center",
         type: "Free transfer",
         eventData: {
           month: "Feb",
-          day: "15",
+          day: "01",
           year: "",
-          time: "Sat - 1:00",
+          time: "Sat - 12:00",
           region: "",
           orderNo: ""
         },
@@ -34,21 +169,21 @@ export const eventData=[
         }
       },
       {
-        artiste: "Tennessee Volunteers Mens Basketball vs. Vanderbilt Commodores Mens Basketball",
+        artiste: "Tennessee Volunteers Mens Basketball vs. Florida Gators Men's Basketball",
         img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
         sec: "230",
         row: "1",
         seat: "13",
-        date: "Sat, Feb 15",
-        time: "1:00pm",
+        date: "Sat, Feb 01",
+        time: "12:00pm",
         state: "Tennessee",
         venue: "Food City Center",
         type: "Free transfer",
         eventData: {
           month: "Feb",
-          day: "15",
+          day: "01",
           year: "",
-          time: "Sat - 1:00",
+          time: "Sat - 12:00",
           region: "",
           orderNo: ""
         },
@@ -67,21 +202,21 @@ export const eventData=[
         }
       }, 
       {
-        artiste: "Tennessee Volunteers Mens Basketball vs. Vanderbilt Commodores Mens Basketball",
+        artiste: "Tennessee Volunteers Mens Basketball vs. Florida Gators Men's Basketball",
         img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
         sec: "230",
         row: "1",
         seat: "14",
-        date: "Sat, Feb 15",
-        time: "1:00pm",
+        date: "Sat, Feb 01",
+        time: "12:00pm",
         state: "Tennessee",
         venue: "Food City Center",
         type: "Free transfer",
         eventData: {
           month: "Feb",
-          day: "15",
+          day: "01",
           year: "",
-          time: "Sat - 1:00",
+          time: "Sat - 12:00",
           region: "",
           orderNo: ""
         },
@@ -100,21 +235,21 @@ export const eventData=[
         }
       },
       {
-        artiste: "Tennessee Volunteers Mens Basketball vs. Vanderbilt Commodores Mens Basketball",
+        artiste: "Tennessee Volunteers Mens Basketball vs. Florida Gators Men's Basketball",
         img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
         sec: "230",
         row: "1",
         seat: "15",
-        date: "Sat, Feb 15",
-        time: "1:00pm",
+        date: "Sat, Feb 01",
+        time: "12:00pm",
         state: "Tennessee",
         venue: "Food City Center",
         type: "Free transfer",
         eventData: {
           month: "Feb",
-          day: "15",
+          day: "01",
           year: "",
-          time: "Sat - 1:00",
+          time: "Sat - 12:00",
           region: "",
           orderNo: ""
         },
@@ -133,141 +268,6 @@ export const eventData=[
         }
       }
     ],
-
-    [
-        {
-          artiste: "Tennessee Volunteers Mens Basketball vs. Florida Gators Men's Basketball",
-          img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
-          sec: "230",
-          row: "1",
-          seat: "12",
-          date: "Sat, Feb 01",
-          time: "12:00pm",
-          state: "Tennessee",
-          venue: "Food City Center",
-          type: "Free transfer",
-          eventData: {
-            month: "Feb",
-            day: "01",
-            year: "",
-            time: "Sat - 12:00",
-            region: "",
-            orderNo: ""
-          },
-          transfers: {
-            sec: "",
-            row: "",
-            seats: ['12','13','14','15'],
-            desc: "200 LEVEL",
-            GA: "",
-            color: "",
-            "venueMap": "",
-            "gate": "",
-            "btnColor": "",
-            sell: "y",
-            trnsfer: "y"
-          }
-        },
-        {
-          artiste: "Tennessee Volunteers Mens Basketball vs. Florida Gators Men's Basketball",
-          img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
-          sec: "230",
-          row: "1",
-          seat: "13",
-          date: "Sat, Feb 01",
-          time: "12:00pm",
-          state: "Tennessee",
-          venue: "Food City Center",
-          type: "Free transfer",
-          eventData: {
-            month: "Feb",
-            day: "01",
-            year: "",
-            time: "Sat - 12:00",
-            region: "",
-            orderNo: ""
-          },
-          transfers: {
-            sec: "",
-            row: "",
-            seats: ['13'],
-            desc: "200 LEVEL",
-            GA: "",
-            color: "",
-            venueMap: "",
-            gate: "",
-            btnColor: "",
-            sell: "",
-            trnsfer: ""
-          }
-        }, 
-        {
-          artiste: "Tennessee Volunteers Mens Basketball vs. Florida Gators Men's Basketball",
-          img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
-          sec: "230",
-          row: "1",
-          seat: "14",
-          date: "Sat, Feb 01",
-          time: "12:00pm",
-          state: "Tennessee",
-          venue: "Food City Center",
-          type: "Free transfer",
-          eventData: {
-            month: "Feb",
-            day: "01",
-            year: "",
-            time: "Sat - 12:00",
-            region: "",
-            orderNo: ""
-          },
-          transfers: {
-            sec: "",
-            row: "",
-            seats: ['14'],
-            desc: "200 LEVEL",
-            GA: "",
-            color: "",
-            "venueMap": "",
-            "gate": "",
-            "btnColor": "",
-            sell: "y",
-            trnsfer: "y"
-          }
-        },
-        {
-          artiste: "Tennessee Volunteers Mens Basketball vs. Florida Gators Men's Basketball",
-          img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
-          sec: "230",
-          row: "1",
-          seat: "15",
-          date: "Sat, Feb 01",
-          time: "12:00pm",
-          state: "Tennessee",
-          venue: "Food City Center",
-          type: "Free transfer",
-          eventData: {
-            month: "Feb",
-            day: "01",
-            year: "",
-            time: "Sat - 12:00",
-            region: "",
-            orderNo: ""
-          },
-          transfers: {
-            sec: "",
-            row: "",
-            seats: ['15'],
-            desc: "200 LEVEL",
-            GA: "",
-            color: "",
-            "venueMap": "",
-            "gate": "",
-            "btnColor": "",
-            sell: "y",
-            trnsfer: "y"
-          }
-        }
-      ],
 
 
 ]
