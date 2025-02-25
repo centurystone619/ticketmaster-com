f831fa12f78b009538d02a41ec4ebe9961b0168d
centurystone619 <michellefoust6@gmail.com>
2025-02-24 18:06:59 -0600

    Update data.js

--- src/data.js
+++ src/data.js
@@ -271,3 +271,275 @@ export const eventData=[
   
   
   ]
+    [
+      {
+        artiste: "Michigan Wolverines Mens Basketball vs. Michigan State Spartans Mens Basketball",
+        img: "https://s1.ticketm.net/dam/a/a34/7a10b110-e36b-4498-80b0-54fdb1791a34_1785751_ARTIST_PAGE_3_2.jpg",
+        sec: "229",
+        row: "28",
+        seat: "16",
+        date: "Fri, Feb 21",
+        time: "8pm",
+        state: "Michigan",
+        venue: "Crisler Center",
+        type: "Resale Ticket",
+        eventData: {
+          month: "Feb",
+          day: "21",
+          year: "",
+          time: "Fri - 8",
+          region: "",
+          orderNo: ""
+        },
+        transfers: {
+          sec: "",
+          row: "",
+          seats: ['16','17','18','19'],
+          desc: "200 LEVEL",
+          GA: "",
+          color: "Blue",
+          venueMap: "https://www.bing.com/maps/geoplat/REST/v1/Imagery/Map/RoadVibrant/35.951168,-83.925026/15?ms=648,183&ml=Basemap,LandCover,Landmarks,OsmBuildings&key=AnTcaqBi2ypp0xI-OZNi4W_ik2KhjgpqioTAtXLC8GzkMBQRMlyxvxyTnd5b73im&c=en-US&fmt=jpeg&od=1&shading=hill&logo=n&da=ro&pp=35.951168,-83.925026;dfp;Thompson-Boling+Arena",
+          "gate": "",
+          "btnColor": "Blue",
+          sell: "y",
+          trnsfer: "y"
+        }
+      },
+      {
+        artiste: "Michigan Wolverines Mens Basketball vs. Michigan State Spartans Mens Basketball",
+        img: "https://s1.ticketm.net/dam/a/a34/7a10b110-e36b-4498-80b0-54fdb1791a34_1785751_ARTIST_PAGE_3_2.jpg",
+        sec: "229",
+        row: "28",
+        seat: "17",
+        date: "Fri, Feb 21",
+        time: "8pm",
+        state: "Michigan",
+        venue: "Crisler Center",
+        type: "Resale Ticket",
+        eventData: {
+          month: "Feb",
+          day: "21",
+          year: "",
+          time: "Fri - 8",
+          region: "",
+          orderNo: ""
+        },
+        transfers: {
+          sec: "",
+          row: "",
+          seats: ['17'],
+          desc: "200 LEVEL",
+          GA: "",
+          color: "Blue",
+          venueMap: "",
+          gate: "",
+          btnColor: "Blue",
+          sell: "",
+          trnsfer: ""
+        }
+      }, 
+      {
+        artiste: "Tennessee Volunteers Mens Basketball vs. Vanderbilt Commodores Mens Basketball",
+        img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
+        sec: "230",
+        row: "1",
+        seat: "14",
+       date: "Sat, Feb 15",
+        time: "1:00pm",
+        state: "Tennessee",
+        venue: "Food City Center",
+        type: "Free transfer",
+        eventData: {
+          month: "Feb",
+          day: "15",
+          year: "",
+          time: "Sat - 1:00",
+          region: "",
+          orderNo: ""
+        },
+        transfers: {
+          sec: "",
+          row: "",
+          seats: ['14'],
+          desc: "200 LEVEL",
+          GA: "",
+          color: "",
+          "venueMap": "",
+          "gate": "",
+          "btnColor": "",
+          sell: "y",
+          trnsfer: "y"
+        }
+      },
+      {
+        artiste: "Tennessee Volunteers Mens Basketball vs. Vanderbilt Commodores Mens Basketball",
+        img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
+        sec: "230",
+        row: "1",
+        seat: "15",
+        date: "Sat, Feb 15",
+        time: "1:00pm",
+        state: "Tennessee",
+        venue: "Food City Center",
+        type: "Free transfer",
+        eventData: {
+          month: "Feb",
+          day: "15",
+          year: "",
+          time: "Sat - 1:00",
+          region: "",
+          orderNo: ""
+        },
+        transfers: {
+          sec: "",
+          row: "",
+          seats: ['15'],
+          desc: "200 LEVEL",
+          GA: "",
+          color: "",
+          "venueMap": "",
+          "gate": "",
+          "btnColor": "",
+          sell: "y",
+          trnsfer: "y"
+        }
+      }
+    ],
+  
+    [
+        {
+          artiste: "Michigan Wolverines Mens Basketball vs. Rutgers Scarlet Knights Mens Basketball",
+          img: "https://s1.ticketm.net/dam/a/a34/7a10b110-e36b-4498-80b0-54fdb1791a34_1785751_ARTIST_PAGE_3_2.jpg",
+          sec: "225",
+          row: "10",
+          seat: "8",
+          date: "Tue, Feb 18",
+          time: "7pm",
+          state: "Michigan",
+          venue: "Jack Breslin Arena",
+          type: "Resale Ticket",
+          eventData: {
+            month: "Feb",
+            day: "18",
+            year: "",
+            time: "Tue - 7",
+            region: "",
+            orderNo: ""
+          },
+          transfers: {
+            sec: "",
+            row: "",
+            seats: ['8','9','10','11'],
+            desc: "200 LEVEL",
+            GA: "",
+            color: "Blue",
+            venueMap: "https://www.bing.com/maps/geoplat/REST/v1/Imagery/Map/RoadVibrant/35.951168,-83.925026/15?ms=648,183&ml=Basemap,LandCover,Landmarks,OsmBuildings&key=AnTcaqBi2ypp0xI-OZNi4W_ik2KhjgpqioTAtXLC8GzkMBQRMlyxvxyTnd5b73im&c=en-US&fmt=jpeg&od=1&shading=hill&logo=n&da=ro&pp=35.951168,-83.925026;dfp;Thompson-Boling+Arena",
+            "gate": "",
+            "btnColor": "Blue",
+            sell: "y",
+            trnsfer: "y"
+          }
+        },
+        {
+          artiste: "Michigan State Spartans Mens Basketball vs. Purdue Boilermakers Mens Basketball",
+          img: "https://s1.ticketm.net/dam/a/446/3b36c8d1-4da6-403c-b608-85af9f305446_1519911_RETINA_PORTRAIT_3_2.jpg",
+          sec: "225",
+          row: "10",
+          seat: "9",
+          date: "Tue, Feb 18",
+          time: "7pm",
+          state: "Michigan",
+          venue: "Jack Breslin Arena",
+          type: "Resale Ticket",
+          eventData: {
+            month: "Feb",
+            day: "18",
+            year: "",
+            time: "Tue - 7",
+            region: "",
+            orderNo: ""
+          },
+          transfers: {
+            sec: "",
+            row: "",
+            seats: ['17'],
+            desc: "200 LEVEL",
+            GA: "",
+            color: "Blue",
+            venueMap: "",
+            gate: "",
+            btnColor: "Blue",
+            sell: "",
+            trnsfer: ""
+          }
+        }, 
+        {
+          artiste: "Men's Basketball vs. Alabama",
+          img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
+          sec: "213",
+          row: "2",
+          seat: "17",
+          date: "Sat, Mar 1",
+          time: "12:00pm",
+          state: "Tennessee",
+          venue: "Food City Center",
+          type: "",
+          eventData: {
+            month: "Mar",
+            day: "01",
+            year: "",
+            time: "Sat - 12:00",
+            region: "",
+            orderNo: ""
+          },
+          transfers: {
+            sec: "",
+            row: "",
+            seats: ['14'],
+            desc: "200 LEVEL",
+            GA: "",
+            color: "",
+            "venueMap": "",
+            "gate": "",
+            "btnColor": "",
+            sell: "y",
+            trnsfer: "y"
+          }
+        },
+        {
+          artiste: "Tennessee Volunteers Mens Basketball vs. Florida Gators Men's Basketball",
+          img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
+          sec: "230",
+          row: "1",
+          seat: "15",
+          date: "Sat, Feb 01",
+          time: "12:00pm",
+          state: "Tennessee",
+          venue: "Food City Center",
+          type: "Free transfer",
+          eventData: {
+            month: "Feb",
+            day: "01",
+            year: "",
+            time: "Sat - 12:00",
+            region: "",
+            orderNo: ""
+          },
+          transfers: {
+            sec: "",
+            row: "",
+            seats: ['15'],
+            desc: "200 LEVEL",
+            GA: "",
+            color: "",
+            "venueMap": "",
+            "gate": "",
+            "btnColor": "",
+            sell: "y",
+            trnsfer: "y"
+          }
+        }
+      ],
+  
+  
+  ]
