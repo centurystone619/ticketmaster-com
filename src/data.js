0512711e36c028a280c64bd907c049d90604d05b
centurystone619 <michellefoust6@gmail.com>
2025-02-27 14:04:21 -0600

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,16 +1,16 @@
 export const eventData=[
   [
     {
-      artiste: "Men's Basketball vs. Alabama",
-        img: "https://raw.githubusercontent.com/centurystone619/Img/refs/heads/main/Sportvenue.JPG",
-        sec: "222",
-        row: "1",
-        seat: "11",
+      artiste: "Tennessee Volunteers Mens Basketball vs. Alabama Crimson Tide",
+        img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
+        sec: "230",
+        row: "2",
+        seat: "12",
         date: "Sat, Mar 1",
         time: "4pm",
         state: "Tennessee",
         venue: "Food City Center",
-        type: "",
+        type: "Resale Ticket",
         eventData: {
           month: "Mar",
           day: "1",
@@ -22,28 +22,28 @@ export const eventData=[
         transfers: {
           sec: "",
           row: "",
-          seats: ['11','12','13','14'],
+          seats: ['12','13','14','15'],
           desc: "200 LEVEL",
           GA: "",
-          color: "Orange",
+          color: "Blue",
           venueMap: "",
           "gate": "",
-          "btnColor": "Orange",
+          "btnColor": "Blue",
           sell: "y",
           trnsfer: "y"
         }
       },
       {
-        artiste: "Men's Basketball vs. Alabama",
-        img: "https://raw.githubusercontent.com/centurystone619/Img/refs/heads/main/Sportvenue.JPG",
-        sec: "222",
-        row: "1",
-        seat: "12",
+        artiste: "Tennessee Volunteers Mens Basketball vs. Alabama Crimson Tide",
+        img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
+        sec: "230",
+        row: "2",
+        seat: "13",
         date: "Sat, Mar 1",
         time: "4pm",
         state: "Tennessee",
         venue: "Food City Center",
-        type: "",
+        type: "Resale Ticket",
         eventData: {
           month: "Mar",
           day: "1",
@@ -58,10 +58,10 @@ export const eventData=[
           seats: ['12'],
           desc: "200 LEVEL",
           GA: "",
-          color: "Orange",
+          color: "Blue",
           venueMap: "",
           gate: "",
-          btnColor: "Orange",
+          btnColor: "Blue",
           sell: "",
           trnsfer: ""
         }
