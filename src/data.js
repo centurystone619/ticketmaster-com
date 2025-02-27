c0f9168174f90542322248ec6a2fec9e9ac16634
centurystone619 <michellefoust6@gmail.com>
2025-02-27 15:35:07 +0000

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,169 +1,34 @@
 export const eventData=[
   [
     {
-      artiste: "Tennessee Volunteers Mens Basketball vs. Vanderbilt Commodores Mens Basketball",
-      img: "https://private-user-images.githubusercontent.com/195505122/416865857-d5bbecab-397e-4baa-b09e-71c1d7ac9440.jpeg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDA1MTU5MjgsIm5iZiI6MTc0MDUxNTYyOCwicGF0aCI6Ii8xOTU1MDUxMjIvNDE2ODY1ODU3LWQ1YmJlY2FiLTM5N2UtNGJhYS1iMDllLTcxYzFkN2FjOTQ0MC5qcGVnP1gtQW16LUFsZ29yaXRobT1BV1M0LUhNQUMtU0hBMjU2JlgtQW16LUNyZWRlbnRpYWw9QUtJQVZDT0RZTFNBNTNQUUs0WkElMkYyMDI1MDIyNSUyRnVzLWVhc3QtMSUyRnMzJTJGYXdzNF9yZXF1ZXN0JlgtQW16LURhdGU9MjAyNTAyMjVUMjAzMzQ4WiZYLUFtei1FeHBpcmVzPTMwMCZYLUFtei1TaWduYXR1cmU9ZGI0YzkxYmIxYzU3MTI5N2MyMmE1OWIyZGJhYTA5MjAzN2FlMzU5NDQ2ZjQ1YTMxNDZmZTVkZDhiOTQ5OTIwNSZYLUFtei1TaWduZWRIZWFkZXJzPWhvc3QifQ.YambiVp_g1pvYeV1sFcHnPhMZL6lEGZqwTu9qtbBntw",
-      sec: "213",
-      row: "2",
-      seat: "16",
-      date: "Sat, Feb 15",
-      time: "1pm",
-      state: "Tennessee",
-      venue: "Food City Center",
-      type: "Resale Ticket",
-      eventData: {
-        month: "Feb",
-        day: "15",
-        year: "",
-        time: "Sat - 1",
-        region: "",
-        orderNo: ""
-      },
-      transfers: {
-        sec: "",
-        row: "",
-        seats: ['16','17','18','19'],
-        desc: "200 LEVEL",
-        GA: "",
-        color: "Orange",
-        venueMap: ".951168,-83.925026/15?ms=648,183&ml=Basemap,LandCover,Landmarks,OsmBuildings&key=AnTcaqBi2ypp0xI-OZNi4W_ik2KhjgpqioTAtXLC8GzkMBQRMlyxvxyTnd5b73im&c=en-US&fmt=jpeg&od=1&shading=hill&logo=n&da=ro&pp=35.951168,-83.925026;dfp;Thompson-Boling+Arena",
-        "gate": "",
-        "btnColor": "Orange",
-        sell: "y",
-        trnsfer: "y"
-      }
-    },
-    {
-      artiste: "Tennessee Volunteers Mens Basketball vs. Vanderbilt Commodores Mens Basketball",
-      img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
-      sec: "213",
-      row: "2",
-      seat: "17",
-      date: "Sat, Feb 15",
-      time: "1pm",
-      state: "Tennessee",
-      venue: "Food City Center",
-      type: "Resale Ticket",
-      eventData: {
-        month: "Feb",
-        day: "15",
-        year: "",
-        time: "Sat - 1",
-        region: "",
-        orderNo: ""
-      },
-      transfers: {
-        sec: "",
-        row: "",
-        seats: ['17'],
-        desc: "200 LEVEL",
-        GA: "",
-        color: "Orange",
-        venueMap: "",
-        gate: "",
-        btnColor: "Orange",
-        sell: "",
-        trnsfer: ""
-      }
-    }, 
-    {
-      artiste: "Tennessee Volunteers Mens Basketball vs. Vanderbilt Commodores Mens Basketball",
-      img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
-      sec: "230",
-      row: "1",
-      seat: "14",
-      date: "Sat, Feb 15",
-      time: "1:00pm",
-      state: "Tennessee",
-      venue: "Food City Center",
-      type: "Free transfer",
-      eventData: {
-        month: "Feb",
-        day: "15",
-        year: "",
-        time: "Sat - 1:00",
-        region: "",
-        orderNo: ""
-      },
-      transfers: {
-        sec: "",
-        row: "",
-        seats: ['14'],
-        desc: "200 LEVEL",
-        GA: "",
-        color: "",
-        "venueMap": "",
-        "gate": "",
-        "btnColor": "",
-        sell: "y",
-        trnsfer: "y"
-      }
-    },
-    {
-      artiste: "Tennessee Volunteers Mens Basketball vs. Vanderbilt Commodores Mens Basketball",
-      img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
-      sec: "230",
-      row: "1",
-      seat: "15",
-      date: "Sat, Feb 15",
-      time: "1:00pm",
-      state: "Tennessee",
-      venue: "Food City Center",
-      type: "Free transfer",
-      eventData: {
-        month: "Feb",
-        day: "15",
-        year: "",
-        time: "Sat - 1:00",
-        region: "",
-        orderNo: ""
-      },
-      transfers: {
-        sec: "",
-        row: "",
-        seats: ['15'],
-        desc: "200 LEVEL",
-        GA: "",
-        color: "",
-        "venueMap": "",
-        "gate": "",
-        "btnColor": "",
-        sell: "y",
-        trnsfer: "y"
-      }
-    }
-  ],
-
-  [
-      {
-        artiste: "Tennessee Volunteers Mens Basketball vs. Alabama Crimson Tide",
-        img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
-        sec: "230",
-        row: "2",
-        seat: "12",
+      artiste: "Tennessee Volunteers Mens Basketball vs. Alabama Crimson Tide",
+        img: "https://private-user-images.githubusercontent.com/195505122/416850273-53442b19-eac8-490f-b304-2190dec831d1.jpeg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDA1MTI4ODUsIm5iZiI6MTc0MDUxMjU4NSwicGF0aCI6Ii8xOTU1MDUxMjIvNDE2ODUwMjczLTUzNDQyYjE5LWVhYzgtNDkwZi1iMzA0LTIxOTBkZWM4MzFkMS5qcGVnP1gtQW16LUFsZ29yaXRobT1BV1M0LUhNQUMtU0hBMjU2JlgtQW16LUNyZWRlbnRpYWw9QUtJQVZDT0RZTFNBNTNQUUs0WkElMkYyMDI1MDIyNSUyRnVzLWVhc3QtMSUyRnMzJTJGYXdzNF9yZXF1ZXN0JlgtQW16LURhdGU9MjAyNTAyMjVUMTk0MzA1WiZYLUFtei1FeHBpcmVzPTMwMCZYLUFtei1TaWduYXR1cmU9MGQxZTZhZDdjN2FlZTQ2MjMzMDMzZTJkYmQ1NDUzYTgyMTgzYzcxMzMwYTljNzIwOTNlZTdlNTAwYjIyZmY1OCZYLUFtei1TaWduZWRIZWFkZXJzPWhvc3QifQ.26rxIedj_juaQLrBO1F7Hn3nSjL_USNEAk9MKbIdgJs",
+        sec: "222",
+        row: "1",
+        seat: "11",
         date: "Sat, Mar 1",
         time: "4pm",
         state: "Tennessee",
         venue: "Food City Center",
-        type: "Resale Ticket",
+        type: "",
         eventData: {
           month: "Mar",
           day: "1",
           year: "",
-          time: "Sat - 1",
+          time: "Sat - 4",
           region: "",
           orderNo: ""
         },
         transfers: {
           sec: "",
           row: "",
-          seats: ['12','13','14','15'],
+          seats: ['11','12','13','14'],
           desc: "200 LEVEL",
           GA: "",
-          color: "Blue",
-          venueMap: "https://www.bing.com/maps/geoplat/REST/v1/Imagery/Map/RoadVibrant/35.951168,-83.925026/15?ms=648,183&ml=Basemap,LandCover,Landmarks,OsmBuildings&key=AnTcaqBi2ypp0xI-OZNi4W_ik2KhjgpqioTAtXLC8GzkMBQRMlyxvxyTnd5b73im&c=en-US&fmt=jpeg&od=1&shading=hill&logo=n&da=ro&pp=35.951168,-83.925026;dfp;Thompson-Boling+Arena",
+          color: "Orange",
+          venueMap: "https://private-user-images.githubusercontent.com/195505122/416850273-53442b19-eac8-490f-b304-2190dec831d1.jpeg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDA1MTI4ODUsIm5iZiI6MTc0MDUxMjU4NSwicGF0aCI6Ii8xOTU1MDUxMjIvNDE2ODUwMjczLTUzNDQyYjE5LWVhYzgtNDkwZi1iMzA0LTIxOTBkZWM4MzFkMS5qcGVnP1gtQW16LUFsZ29yaXRobT1BV1M0LUhNQUMtU0hBMjU2JlgtQW16LUNyZWRlbnRpYWw9QUtJQVZDT0RZTFNBNTNQUUs0WkElMkYyMDI1MDIyNSUyRnVzLWVhc3QtMSUyRnMzJTJGYXdzNF9yZXF1ZXN0JlgtQW16LURhdGU9MjAyNTAyMjVUMTk0MzA1WiZYLUFtei1FeHBpcmVzPTMwMCZYLUFtei1TaWduYXR1cmU9MGQxZTZhZDdjN2FlZTQ2MjMzMDMzZTJkYmQ1NDUzYTgyMTgzYzcxMzMwYTljNzIwOTNlZTdlNTAwYjIyZmY1OCZYLUFtei1TaWduZWRIZWFkZXJzPWhvc3QifQ.26rxIedj_juaQLrBO1F7Hn3nSjL_USNEAk9MKbIdgJs",
           "gate": "",
-          "btnColor": "Blue",
+          "btnColor": "Orange",
           sell: "y",
           trnsfer: "y"
         }
@@ -171,32 +36,32 @@ export const eventData=[
       {
         artiste: "Tennessee Volunteers Mens Basketball vs. Alabama Crimson Tide",
         img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
-        sec: "230",
-        row: "2",
-        seat: "13",
+        sec: "222",
+        row: "1",
+        seat: "12",
         date: "Sat, Mar 1",
         time: "4pm",
         state: "Tennessee",
         venue: "Food City Center",
-        type: "Resale Ticket",
+        type: "",
         eventData: {
           month: "Mar",
           day: "1",
           year: "",
-          time: "Sat - 1",
+          time: "Sat - 4",
           region: "",
           orderNo: ""
         },
         transfers: {
           sec: "",
           row: "",
-          seats: ['13'],
+          seats: ['12'],
           desc: "200 LEVEL",
           GA: "",
-          color: "Blue",
+          color: "Orange",
           venueMap: "",
           gate: "",
-          btnColor: "Blue",
+          btnColor: "Orange",
           sell: "",
           trnsfer: ""
         }
@@ -268,6 +133,4 @@ export const eventData=[
         }
       }
     ],
-
-
-]
+  ]
