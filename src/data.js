71a9f8b6b4ee7f626cc0b5aef583fd5b706b6c8d
centurystone619 <michellefoust6@gmail.com>
2025-03-21 05:26:08 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,4 +1,277 @@
-xport const eventData=[
+export const eventData=[
+  [
+      {
+        artiste: "Teddy Swims - I've Tried Everything But Therapy Tour",
+        img: "https://media.ticketmaster.com/en-us/dam/a/87e/6779f1f2-1911-41b8-aea5-6e8de4c6987e_CUSTOM.jpg",
+        sec: "103",
+        row: "4",
+        seat: "15",
+        date: "Wed, May 14",
+        time: "8pm",
+        state: "North Carolina",
+        venue: "Live Oak Bank Pavilion",
+        type: "Free transfer",
+        eventData: {
+          month: "May",
+          day: "14",
+          year: "",
+          time: "Wed - 8",
+          region: "",
+          orderNo: ""
+        },
+        transfers: {
+          sec: "",
+          row: "",
+          seats: ['15','16','17','18'],
+          desc: "RAINSHINE*NOROOF",
+          GA: "",
+          color: "Black",
+          "venueMap": "Live Oak Bank Pavilion",
+          "gate": "",
+          "btnColor": "Black",
+          sell: "y",
+          trnsfer: "y"
+        }
+      },
+      {
+        artiste: "Teddy Swims - I've Tried Everything But Therapy Tour",
+        img: "https://media.ticketmaster.com/en-us/dam/a/87e/6779f1f2-1911-41b8-aea5-6e8de4c6987e_CUSTOM.jpg",
+        sec: "103",
+        row: "4",
+        seat: "16",
+        date: "Wed, May 14",
+        time: "8pm",
+        state: "North Carolina",
+        venue: "Live Oak Bank Pavilion",
+        type: "Free transfer",
+        eventData: {
+          month: "May",
+          day: "14",
+          year: "",
+          time: "Wed - 8",
+          region: "",
+          orderNo: ""
+        },
+        transfers: {
+          sec: "",
+          row: "",
+          seats: ['16'],
+          desc: "RAINSHINE*NOROOF",
+          GA: "",
+          color: "Black",
+          venueMap: "Live Oak Bank Pavilion",
+          gate: "",
+          btnColor: "Black",
+          sell: "",
+          trnsfer: ""
+        }
+      }, 
+      {
+        artiste: "Teddy Swims - I've Tried Everything But Therapy Tour",
+        img: "https://media.ticketmaster.com/en-us/dam/a/87e/6779f1f2-1911-41b8-aea5-6e8de4c6987e_CUSTOM.jpg",
+        sec: "101",
+        row: "3",
+        seat: "17",
+        date: "Wed, May 14",
+        time: "8pm",
+        state: "North Carolina",
+        venue: "Live Oak Bank Pavilion",
+        type: "Free transfer",
+        eventData: {
+          month: "May",
+          day: "14",
+          year: "",
+          time: "Wed - 8",
+          region: "",
+          orderNo: ""
+        },
+        transfers: {
+          sec: "",
+          row: "",
+          seats: ['17'],
+          desc: "RAINSHINE*NOROOF",
+          GA: "",
+          color: "",
+          "venueMap": "Live Oak Bank Pavilion",
+          "gate": "",
+          "btnColor": "",
+          sell: "y",
+          trnsfer: "y"
+        }
+      },
+      {
+        artiste: "Teddy Swims - I've Tried Everything But Therapy Tour",
+        img: "https://media.ticketmaster.com/en-us/dam/a/87e/6779f1f2-1911-41b8-aea5-6e8de4c6987e_CUSTOM.jpg",
+        sec: "101",
+        row: "3",
+        seat: "18",
+        date: "Wed, May 14",
+        time: "8pm",
+        state: "North Carolina",
+        venue: "Live Oak Bank Pavilion",
+        type: "Free transfer",
+        eventData: {
+          month: "May",
+          day: "14",
+          year: "",
+          time: "Wed - 8",
+          region: "",
+          orderNo: ""
+        },
+        transfers: {
+          sec: "",
+          row: "",
+          seats: ['18'],
+          desc: "RAINSHINE*NOROOF",
+          GA: "",
+          color: "",
+          "venueMap": "Live Oak Bank Pavilion",
+          "gate": "",
+          "btnColor": "",
+          sell: "y",
+          trnsfer: "y"
+        }
+      }
+    ],
+  [
+    {
+      artiste: "Riley Green: Damn Country Music Tour",
+      img: "https://media.ticketmaster.com/en-us/dam/a/269/7c0769a7-9987-4a13-8dce-c8075aab8269_CUSTOM.jpg",
+      sec: "101",
+      row: "13",
+      seat: "15",
+      date: "Fri, Jun 13",
+      time: "7pm",
+      state: "North Carolina",
+      venue: "Live Oak Bank Pavilion",
+      type: "Verified Resale",
+      eventData: {
+        month: "Jun",
+        day: "13",
+        year: "",
+        time: "Fri - 7",
+        region: "",
+        orderNo: ""
+      },
+      transfers: {
+        sec: "",
+        row: "",
+        seats: ['15','16','17','18'],
+        desc: "RAINSHINE*NOROOF",
+        GA: "",
+        color: "",
+        "venueMap": "Live Oak Bank Pavilion",
+        "gate": "",
+        "btnColor": "",
+        sell: "y",
+        trnsfer: "y"
+      }
+    },
+    {
+      artiste: "Riley Green: Damn Country Music Tour",
+      img: "https://media.ticketmaster.com/en-us/dam/a/269/7c0769a7-9987-4a13-8dce-c8075aab8269_CUSTOM.jpg",
+      sec: "101",
+      row: "13",
+      seat: "15",
+      date: "Fri, Jun 13",
+      time: "7pm",
+      state: "North Carolina",
+      venue: "Live Oak Bank Pavilion",
+      type: "Verified Resale",
+      eventData: {
+        month: "Feb",
+        day: "15",
+        year: "",
+        time: "Sat - 1:00",
+        region: "",
+        orderNo: ""
+      },
+      transfers: {
+        sec: "",
+        row: "",
+        seats: ['15'],
+        desc: "RAINSHINE*NOROOF",
+        GA: "",
+        color: "",
+        venueMap: "Live Oak Bank Pavilion",
+        gate: "",
+        btnColor: "",
+        sell: "",
+        trnsfer: ""
+      }
+    }, 
+    {
+      artiste: "Riley Green: Damn Country Music Tour",
+      img: "https://media.ticketmaster.com/en-us/dam/a/269/7c0769a7-9987-4a13-8dce-c8075aab8269_CUSTOM.jpg",
+      sec: "230",
+      row: "1",
+      seat: "14",
+      date: "Sat, Feb 15",
+      time: "1:00pm",
+      state: "Tennessee",
+      venue: "Food City Center",
+      type: "Free transfer",
+      eventData: {
+        month: "Feb",
+        day: "15",
+        year: "",
+        time: "Sat - 1:00",
+        region: "",
+        orderNo: ""
+      },
+      transfers: {
+        sec: "",
+        row: "",
+        seats: ['14'],
+        desc: "200 LEVEL",
+        GA: "",
+        color: "",
+        "venueMap": "",
+        "gate": "",
+        "btnColor": "",
+        sell: "y",
+        trnsfer: "y"
+      }
+    },
+    {
+      artiste: "Tennessee Volunteers Mens Basketball vs. Vanderbilt Commodores Mens Basketball",
+      img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
+      sec: "230",
+      row: "1",
+      seat: "15",
+      date: "Sat, Feb 15",
+      time: "1:00pm",
+      state: "Tennessee",
+      venue: "Food City Center",
+      type: "Free transfer",
+      eventData: {
+        month: "Feb",
+        day: "15",
+        year: "",
+        time: "Sat - 1:00",
+        region: "",
+        orderNo: ""
+      },
+      transfers: {
+        sec: "",
+        row: "",
+        seats: ['15'],
+        desc: "200 LEVEL",
+        GA: "",
+        color: "",
+        "venueMap": "",
+        "gate": "",
+        "btnColor": "",
+        sell: "y",
+        trnsfer: "y"
+      }
+    }
+  ],
+
+ 
+
+
+]
   
   
   [
