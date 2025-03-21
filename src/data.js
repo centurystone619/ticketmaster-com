a1f81bde68738fbc8e3994eaa8b8f9bad156ca83
centurystone619 <michellefoust6@gmail.com>
2025-03-21 05:14:25 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,98 +1,32 @@
-export const eventData=[
+xport const eventData=[
   [
-    {
-      artiste: "2025 SEC Men's Basketball Tournament Session 7",
-        img: "https://media.ticketmaster.com/en-us/dam/a/c33/b05330a9-3662-4376-89ba-e6c0442f5c33_CUSTOM.jpg",
-        sec: "107",
-        row: "C",
-        seat: "7",
-        date: "Sat, Mar 15",
-        time: "12pm",
-        state: "Tennessee",
-        venue: "Bridgestone Arena",
-        type: "Resale Ticket",
+      {
+        artiste: "Teddy Swims - I've Tried Everything But Therapy Tour",
+        img: "https://media.ticketmaster.com/en-us/dam/a/87e/6779f1f2-1911-41b8-aea5-6e8de4c6987e_CUSTOM.jpg",
+        sec: "101",
+        row: "3",
+        seat: "19",
+        date: "Wed, May 14",
+        time: "8pm",
+        state: "North Carolina",
+        venue: "Live Oak Bank Pavilion",
+        type: "Free transfer",
         eventData: {
-          month: "Mar",
+          month: "May",
           day: "14",
           year: "",
-          time: "Sat - 12",
+          time: "Wed - 8",
           region: "",
           orderNo: ""
         },
         transfers: {
           sec: "",
           row: "",
-          seats: ['7','8','9','10'],
-          desc: "",
-          GA: "",
-          color: "Blue",
-          venueMap: "https://www.bing.com/maps/geoplat/REST/v1/Imagery/Map/RoadVibrant/36.159164,-86.778503/16?ms=1182,334&ml=Basemap,LandCover,Landmarks,OsmBuildings&key=AnTcaqBi2ypp0xI-OZNi4W_ik2KhjgpqioTAtXLC8GzkMBQRMlyxvxyTnd5b73im&c=en-US&dpi=Large&fmt=jpeg&od=1&shading=hill&logo=n&da=ro&pp=36.159164,-86.778503;dfp;Bridgestone+Arena",
-          "gate": "",
-          "btnColor": "Blue",
-          sell: "y",
-          trnsfer: "y"
-        }
-      },
-      {
-        artiste: "2025 SEC Men's Basketball Tournament Session 7",
-        img: "https://media.ticketmaster.com/en-us/dam/a/c33/b05330a9-3662-4376-89ba-e6c0442f5c33_CUSTOM.jpg",
-        sec: "107",
-        row: "C",
-        seat: "8",
-        date: "Sat, Mar 15",
-        time: "12pm",
-        state: "Tennessee",
-        venue: "Bridgestone Arena",
-        type: "Resale Ticket",
-        eventData: {
-          month: "Mar",
-          day: "15",
-          year: "",
-          time: "Sat - 12",
-          region: "",
-          orderNo: ""
-        },
-        transfers: {
-          sec: "",
-          row: "",
-          seats: ['8'],
-          desc: "",
-          GA: "",
-          color: "Blue",
-          venueMap: "https://www.bing.com/maps/geoplat/REST/v1/Imagery/Map/RoadVibrant/36.159164,-86.778503/16?ms=1182,334&ml=Basemap,LandCover,Landmarks,OsmBuildings&key=AnTcaqBi2ypp0xI-OZNi4W_ik2KhjgpqioTAtXLC8GzkMBQRMlyxvxyTnd5b73im&c=en-US&dpi=Large&fmt=jpeg&od=1&shading=hill&logo=n&da=ro&pp=36.159164,-86.778503;dfp;Bridgestone+Arena",
-          gate: "",
-          btnColor: "Blue",
-          sell: "",
-          trnsfer: ""
-        }
-      }, 
-      {
-        artiste: "Men's Basketball vs. Alabama",
-        img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
-        sec: "213",
-        row: "2",
-        seat: "17",
-        date: "Sat, Mar 1",
-        time: "12:00pm",
-        state: "Tennessee",
-        venue: "Food City Center",
-        type: "",
-        eventData: {
-          month: "Mar",
-          day: "01",
-          year: "",
-          time: "Sat - 12:00",
-          region: "",
-          orderNo: ""
-        },
-        transfers: {
-          sec: "",
-          row: "",
-          seats: ['14'],
-          desc: "200 LEVEL",
+          seats: ['19'],
+          desc: "RAINSHINE*NOROOF",
           GA: "",
           color: "",
-          "venueMap": "",
+          "venueMap": "Live Oak Bank Pavilion",
           "gate": "",
           "btnColor": "",
           sell: "y",
@@ -100,32 +34,32 @@ export const eventData=[
         }
       },
       {
-        artiste: "Tennessee Volunteers Mens Basketball vs. Florida Gators Men's Basketball",
-        img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
-        sec: "230",
-        row: "1",
-        seat: "15",
-        date: "Sat, Feb 01",
-        time: "12:00pm",
-        state: "Tennessee",
-        venue: "Food City Center",
+        artiste: "Teddy Swims - I've Tried Everything But Therapy Tour",
+        img: "https://media.ticketmaster.com/en-us/dam/a/87e/6779f1f2-1911-41b8-aea5-6e8de4c6987e_CUSTOM.jpg",
+        sec: "101",
+        row: "3",
+        seat: "20",
+        date: "Wed, May 14",
+        time: "8pm",
+        state: "North Carolina",
+        venue: "Live Oak Bank Pavilion",
         type: "Free transfer",
         eventData: {
-          month: "Feb",
-          day: "01",
+          month: "May",
+          day: "14",
           year: "",
-          time: "Sat - 12:00",
+          time: "Wed - 8",
           region: "",
           orderNo: ""
         },
         transfers: {
           sec: "",
           row: "",
-          seats: ['15'],
-          desc: "200 LEVEL",
+          seats: ['20'],
+          desc: "RAINSHINE*NOROOF",
           GA: "",
           color: "",
-          "venueMap": "",
+          "venueMap": "Live Oak Bank Pavilion",
           "gate": "",
           "btnColor": "",
           sell: "y",
@@ -133,4 +67,142 @@ export const eventData=[
         }
       }
     ],
-  ]
+  [
+    {
+      artiste: "Clemson Tigers Baseball vs. South Carolina Gamecocks Baseball",
+      img: "https://media.ticketmaster.com/en-us/dam/a/ed8/594ca3e9-7b5a-4800-a3c7-f9d3d3b77ed8_CUSTOM.jpg",
+      sec: "103",
+      row: "G",
+      seat: "5",
+      date: "Sat, Mar 1",
+      time: "1:30pm",
+      state: "South Carolina",
+      venue: "Fluor Field",
+      type: "Resale Ticket",
+      eventData: {
+        month: "Mar",
+        day: "1",
+        year: "",
+        time: "Sat - 1:30",
+        region: "",
+        orderNo: ""
+      },
+      transfers: {
+        sec: "",
+        row: "",
+        seats: ['5','6','7','8'],
+        desc: "100 Level",
+        GA: "",
+        color: "",
+        venueMap: "https://www.bing.com/images/search?view=detailV2&ccid=P7b3QBIh&id=A6E242E841771ADB104555733CA424D1AFB037D0&thid=OIP.P7b3QBIhzJy-RcxkjHNELAHaHa&mediaurl=https%3a%2f%2fvenue-images.boldticket.com%2f1849-large-v2.png&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.3fb6f7401221cc9cbe45cc648c73442c%3frik%3d0Dewr9EkpDxzVQ%26pid%3dImgRaw%26r%3d0&exph=1280&expw=1280&q=WARNER+THEATER+DC+MAP&simid=608052728811233831&FORM=IRPRST&ck=C1CB6B079595E02E69EAE5A4CE26E4FD&selectedIndex=34&itb=0",
+        "gate": "",
+        "btnColor": "",
+        sell: "y",
+        trnsfer: "y"
+      }
+    },
+    {
+      artiste: "Clemson Tigers Baseball vs. South Carolina Gamecocks Baseball",
+      img: "https://media.ticketmaster.com/en-us/dam/a/ed8/594ca3e9-7b5a-4800-a3c7-f9d3d3b77ed8_CUSTOM.jpg",
+      sec: "103",
+      row: "G",
+      seat: "6",
+      date: "Sat, Mar 1",
+      time: "8pm",
+      state: "South Carolina",
+      venue: "Fluor Field",
+      type: "Resale Ticket",
+      eventData: {
+        month: "Mar",
+        day: "1",
+        year: "",
+        time: "Sat - 1:30",
+        region: "",
+        orderNo: ""
+      },
+      transfers: {
+        sec: "",
+        row: "",
+        seats: ['6'],
+        desc: "",
+        GA: "",
+        color: "",
+        venueMap: "https://www.bing.com/images/search?view=detailV2&ccid=P7b3QBIh&id=A6E242E841771ADB104555733CA424D1AFB037D0&thid=OIP.P7b3QBIhzJy-RcxkjHNELAHaHa&mediaurl=https%3a%2f%2fvenue-images.boldticket.com%2f1849-large-v2.png&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.3fb6f7401221cc9cbe45cc648c73442c%3frik%3d0Dewr9EkpDxzVQ%26pid%3dImgRaw%26r%3d0&exph=1280&expw=1280&q=WARNER+THEATER+DC+MAP&simid=608052728811233831&FORM=IRPRST&ck=C1CB6B079595E02E69EAE5A4CE26E4FD&selectedIndex=34&itb=0",
+        gate: "",
+        btnColor: "",
+        sell: "",
+        trnsfer: ""
+      }
+    }, 
+    {
+      artiste: "Riley Green: Damn Country Music Tour",
+      img: "https://media.ticketmaster.com/en-us/dam/a/269/7c0769a7-9987-4a13-8dce-c8075aab8269_CUSTOM.jpg",
+      sec: "230",
+      row: "1",
+      seat: "14",
+      date: "Sat, Feb 15",
+      time: "1:00pm",
+      state: "Tennessee",
+      venue: "Food City Center",
+      type: "Free transfer",
+      eventData: {
+        month: "Feb",
+        day: "15",
+        year: "",
+        time: "Sat - 1:00",
+        region: "",
+        orderNo: ""
+      },
+      transfers: {
+        sec: "",
+        row: "",
+        seats: ['14'],
+        desc: "200 LEVEL",
+        GA: "",
+        color: "",
+        "venueMap": "",
+        "gate": "",
+        "btnColor": "",
+        sell: "y",
+        trnsfer: "y"
+      }
+    },
+    {
+      artiste: "Tennessee Volunteers Mens Basketball vs. Vanderbilt Commodores Mens Basketball",
+      img: "https://media.ticketmaster.com/en-us/dam/a/ad5/a699aaef-d0e1-4609-b140-91535fc4aad5_1685901_CUSTOM.jpg",
+      sec: "230",
+      row: "1",
+      seat: "15",
+      date: "Sat, Feb 15",
+      time: "1:00pm",
+      state: "Tennessee",
+      venue: "Food City Center",
+      type: "Free transfer",
+      eventData: {
+        month: "Feb",
+        day: "15",
+        year: "",
+        time: "Sat - 1:00",
+        region: "",
+        orderNo: ""
+      },
+      transfers: {
+        sec: "",
+        row: "",
+        seats: ['15'],
+        desc: "200 LEVEL",
+        GA: "",
+        color: "",
+        "venueMap": "",
+        "gate": "",
+        "btnColor": "",
+        sell: "y",
+        trnsfer: "y"
+      }
+    }
+  ],
+
+ 
+
+
+]
