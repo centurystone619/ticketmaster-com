85e2dddde980befbacfe7e65f479eb1e05931fcf
centurystone619 <michellefoust6@gmail.com>
2025-03-30 18:23:47 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,73 +1,44 @@
 export const eventData=[
     [
-        // {
-        //   artiste: "2025 NCAA Men's Basketball Championship - South Regional",
-        //   img: "https://media.ticketmaster.com/en-us/dam/a/b80/6c99ea06-da08-4213-97d1-49f351fd9b80_1512001_CUSTOM.jpg",
-        //   sec: "102",
-        //   row: "R",
-        //   seat: "7",
-        //   date: "Fri, Mar 28",
-        //   time: "7:09pm",
-        //   state: "Georgia",
-        //   venue: "State Farm Arena",
-        //   type: "Free transfer",
-        //   eventData: {
-        //     month: "Mar",
-        //     day: "28",
-        //     year: "",
-        //     time: "Fri - 7:09",
-        //     region: "",
-        //     orderNo: ""
-        //   },
-        //   transfers: {
-        //     sec: "",
-        //     row: "",
-        //     seats: ['7','8','9','10'],
-        //     desc: "LOWER LEVEL",
-        //     GA: "",
-        //     color: "Black",
-        //     "venueMap": "https://www.bing.com/maps/geoplat/REST/v1/Imagery/Map/RoadVibrant/33.757385,-84.396255/16?ms=1182,334&ml=Basemap,LandCover,Landmarks,OsmBuildings&key=AnTcaqBi2ypp0xI-OZNi4W_ik2KhjgpqioTAtXLC8GzkMBQRMlyxvxyTnd5b73im&c=en-US&dpi=Large&fmt=jpeg&od=1&shading=hill&logo=n&da=ro&pp=33.757385,-84.396255;dfp;State+Farm+Arena",
-        //     "gate": "",
-        //     "btnColor": "Black",
-        //     sell: "y",
-        //     trnsfer: "y"
-        //   }
-        // },
-        // {
-        //   artiste: "2025 NCAA Men's Basketball Championship - South Regional",
-        //   img: "https://media.ticketmaster.com/en-us/dam/a/b80/6c99ea06-da08-4213-97d1-49f351fd9b80_1512001_CUSTOM.jpg",
-        //   sec: "102",
-        //   row: "R",
-        //   seat: "8",
-        //   date: "Fri, Mar 28",
-        //   time: "7:09pm",
-        //   state: "Georgia",
-        //   venue: "State Farm Arena",
-        //   type: "Free transfer",
-        //   eventData: {
-        //     month: "Mar",
-        //     day: "28",
-        //     year: "",
-        //     time: "Fri - 7:09",
-        //     region: "",
-        //     orderNo: ""
-        //   },
-        //   transfers: {
-        //     sec: "",
-        //     row: "",
-        //     seats: ['7','8','9','10'],
-        //     desc: "LOWER LEVEL",
-        //     GA: "",
-        //     color: "Black",
-        //     "venueMap": "https://www.bing.com/maps/geoplat/REST/v1/Imagery/Map/RoadVibrant/33.757385,-84.396255/16?ms=1182,334&ml=Basemap,LandCover,Landmarks,OsmBuildings&key=AnTcaqBi2ypp0xI-OZNi4W_ik2KhjgpqioTAtXLC8GzkMBQRMlyxvxyTnd5b73im&c=en-US&dpi=Large&fmt=jpeg&od=1&shading=hill&logo=n&da=ro&pp=33.757385,-84.396255;dfp;State+Farm+Arena",
-        //     "gate": "",
-        //     "btnColor": "Black",
-        //     sell: "y",
-        //     trnsfer: "y"
-        //   }
-        // },
+
+          {
+          artiste: "2025 NCAA Men's Basketball Championship - South Regional",
+          img: "https://media.ticketmaster.com/en-us/dam/a/b80/6c99ea06-da08-4213-97d1-49f351fd9b80_1512001_CUSTOM.jpg",
+          sec: "102",
+          row: "R",
+          seat: "7",
+          date: "Fri, Mar 28",
+          time: "7:09pm",
+          state: "Georgia",
+          venue: "State Farm Arena",
+          type: "Free transfer",
+          eventData: {
+            month: "Mar",
+            day: "28",
+            year: "",
+            time: "Fri - 7:09",
+            region: "",
+            orderNo: ""
+          },
+          transfers: {
+            sec: "",
+            row: "",
+            seats: ['7','8','9','10'],
+            desc: "LOWER LEVEL",
+            GA: "",
+            color: "Black",
+            "venueMap": "https://www.bing.com/maps/geoplat/REST/v1/Imagery/Map/RoadVibrant/33.757385,-84.396255/16?ms=1182,334&ml=Basemap,LandCover,Landmarks,OsmBuildings&key=AnTcaqBi2ypp0xI-OZNi4W_ik2KhjgpqioTAtXLC8GzkMBQRMlyxvxyTnd5b73im&c=en-US&dpi=Large&fmt=jpeg&od=1&shading=hill&logo=n&da=ro&pp=33.757385,-84.396255;dfp;State+Farm+Arena",
+            "gate": "",
+            "btnColor": "Black",
+            sell: "y",
+            trnsfer: "y"
+          }
+        },
+    ],
+    [
+       
          {
-          artiste: "Teddy Swims - I've Tried Everything But Therapy Tour",
+             artiste: "Teddy Swims - I've Tried Everything But Therapy Tour",
           img: "https://media.ticketmaster.com/en-us/dam/a/87e/6779f1f2-1911-41b8-aea5-6e8de4c6987e_CUSTOM.jpg",
           sec: "103",
           row: "10",
