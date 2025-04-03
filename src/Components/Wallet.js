cc2ad8f5326b66b9a969e8400feec1fd6adccfd5
centurystone619 <michellefoust6@gmail.com>
2025-04-03 21:08:27 +0100

    Add files via upload

--- src/Components/Wallet.js
+++ src/Components/Wallet.js
@@ -4,23 +4,42 @@ import Tixmast from '../assets/ticketmasterlogonew.svg';
 import volumebg from '../assets/bg-volume.png';
 import Screenreader from '../assets/screenreaderbg.png';
 import IndexBtn from './IndexBtn';
+import logoImg from '../assets/tml.PNG'
 
 const Wallet = ({ setIsWalletOpen, eventIndex, seatIndex, handleTicketbtn }) => {
   const walletWrapperRef = useRef(null);
 
   // Get color style based on transfers
+  // const getColorClass = () => {
+  //   const firstEvent = eventIndex?.[0];
+  //   if (!firstEvent) return '#004EE7'; // Fallback color
+  //   if (firstEvent.transfers?.GA && !firstEvent.transfers?.color) {
+  //     return '#004EE7';
+  //   } else if (!firstEvent.transfers?.GA && !firstEvent.transfers?.color) {
+  //     return '#004EE7';
+  //   }
+  //    else {
+  //     return firstEvent.transfers?.color;
+  //   }
+
+   
+  // };
+
+
   const getColorClass = () => {
     const firstEvent = eventIndex?.[0];
-    if (!firstEvent) return '#004EE7'; // Fallback color
+    if (!firstEvent) return '#20252f'; // Fallback color
     if (firstEvent.transfers?.GA && !firstEvent.transfers?.color) {
-      return '#004EE7';
+      return '#20252f';
     } else if (!firstEvent.transfers?.GA && !firstEvent.transfers?.color) {
-      return '#004EE7';
-    } else {
+      return '#20252f';
+    }
+     else {
       return firstEvent.transfers?.color;
     }
-  };
 
+   
+  };
   const colorStyle = getColorClass();
 
   // TranslateX calculation
@@ -105,29 +124,57 @@ const Wallet = ({ setIsWalletOpen, eventIndex, seatIndex, handleTicketbtn }) => 
                 </div>
                 <div className="w-5/12">
                   <ul className="px-5 -space-y-1">
-                    <li id="walletTitle" className="text-end">
+                    <li id="walletTitle" className={`${t.transfers?.walletBlank? 'text-start text-[#006ce6] font-[800]' : 'text-end font-[500]'}`}>
                       {t.time}
                     </li>
-                    <li id="walletInfo" className="text-end">
+                    <li id="walletInfo" className={`${t.transfers?.walletBlank? 'text-start' : 'text-end'}`}>
                       {t?.eventData?.month} {t?.eventData?.day}, {t?.eventData?.year}
                     </li>
                   </ul>
                 </div>
               </div>
-              <div
+
+{t.transfers.walletBlank? (
+  <div
+  style={{
+    backgroundImage: `url(${logoImg}) `,
+    // backgroundImage: `${t.transfers.walletBlank? <img src={logoImg} alt='img'/> : `url(${t.img})`} `,
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center 50%',
+    backgroundColor:'#006ce6'
+  }}
+  className="h-20 w-full"
+></div>
+):(
+  <div
+  style={{
+    backgroundImage: `url(${t.img}) `,
+    // backgroundImage: `${t.transfers.walletBlank? <img src={logoImg} alt='img'/> : `url(${t.img})`} `,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center 5%',
+  }}
+  className="h-20 w-full"
+></div>
+)}
+
+              {/* <div
                 style={{
-                  backgroundImage: `url(${t.img})`,
+                  backgroundImage: `url(${t.img}) `,
+                  // backgroundImage: `${t.transfers.walletBlank? <img src={logoImg} alt='img'/> : `url(${t.img})`} `,
                   backgroundSize: 'cover',
                   backgroundRepeat: 'no-repeat',
                   backgroundPosition: 'center 5%',
                 }}
                 className="h-20 w-full"
-              ></div>
+              ></div> */}
               <div className="flex flex-col justify-between">
-                <ul className="text-azure-white mx-5 space-y-3 pt-2 text-wrap">
+                <ul className="text-azure-white mx-4 space-y-3 pt-2 text-wrap">
                   <li>
-                    <p id="walletTitle">{t.venue}</p>
-                    <p
+                    {/* <p id="walletTitle">{t.venue}</p> */}
+                    {t.transfers.walletBlank? (<p id="blankTitle">{t.venue}</p>):(<p id="walletTitle" className='font-[500]'>{t.venue}</p>) }
+                    {/* <p
                       id="walletInfo"
                       className="w-full leading-none"
                       style={{
@@ -137,46 +184,143 @@ const Wallet = ({ setIsWalletOpen, eventIndex, seatIndex, handleTicketbtn }) => 
                       }}
                     >
                       {t.artiste}
-                    </p>
+                    </p> */}
+                    {t.transfers.walletBlank?  
+                    <p
+                      id="blankInfo"
+                      className="w-full leading-none"
+                      style={{
+                        overflowWrap: 'break-word',
+                        wordBreak: 'break-word',
+                        whiteSpace: 'normal',
+                      }}
+                    >
+                      {t.artiste}
+                    </p> : 
+                     <p
+                      id="walletInfo"
+                      className="w-full leading-none"
+                      style={{
+                        overflowWrap: 'break-word',
+                        wordBreak: 'break-word',
+                        whiteSpace: 'normal',
+                      }}
+                    >
+                      {t.artiste}
+                    </p> }
                   </li>
                   <li>
                     {t.transfers?.GA ? (
                       <ul className="flex item-center justify-around">
                         <li>
-                          <p id="walletTitle">SECTION</p>
+                          <p id="walletTitle" >SECTION</p>
                           <p id="walletInfo">{t.sec}</p>
                         </li>
+
+                        
                         <li id="walletInfo" className="flex m-auto">
                           {t.transfers?.GA}
                         </li>
                       </ul>
                     ) : (
                       <ul className="flex justify-between items-center">
-                        <li>
+                        {/* <li>
                           <p id="walletTitle">SECTION</p>
                           <p id="walletInfo">{t.sec}</p>
-                        </li>
-                        <li>
+                        </li> */}
+
+                        {t.transfers?.walletBlank?( <li>
+                          <p id="blankSec">SECTION</p>
+                          <p id="walletInfoSec" >{t.sec}</p>
+                        </li>):(
+                           <li>
+                           <p id="walletTitle">SECTION</p>
+                           <p id="walletInfo">{t.sec}</p>
+                         </li>
+                        )}
+                        {/* <li>
+                          <p id="walletTitle">ROW</p>
+                          <p id="walletInfo">{t.row}</p>
+                        </li> */}
+
+                        {t.transfers?.walletBlank?(
+ <li className='text-center'>
+ <p id="blankSec">ROW</p>
+ <p id="walletInfoSec">{t.row}</p>
+</li>
+                        ):(
+                          <li>
                           <p id="walletTitle">ROW</p>
                           <p id="walletInfo">{t.row}</p>
                         </li>
-                        <li>
+                        )}
+                        {/* <li>
+                          <p id="walletTitle">SEAT</p>
+                          <p id="walletInfo">{t.seat}</p>
+                        </li> */}
+
+                        {t.transfers?.walletBlank?(
+                            <li className='text-end'>
+                            <p id="blankSec">SEAT</p>
+                            <p id="walletInfoSec">{t.seat}</p>
+                          </li>
+                        ):(
+                          <li>
                           <p id="walletTitle">SEAT</p>
                           <p id="walletInfo">{t.seat}</p>
                         </li>
+                        )}
                       </ul>
                     )}
                   </li>
                   <li>
-                  {t.transfers?.gate? (<div><p id="walletTitle">ENTRY INFO</p>
+                  {/* {t.transfers?.gate? (<div>
+                    {t.transfers?.walletBlank?(               
+                           <p id="blankSec">ENTRY INFO</p>
+):(                   
+                       <p id="walletTitle">ENTRY INFO</p>
+)}
                     <p id="walletInfo">{t.transfers?.gate}</p></div>) : (<div><p id="walletTitle">ENTRY INFO</p>
-                      <p id="walletInfo">{t.transfers?.desc}</p></div>) }
+                      <p id="walletInfo">{t.transfers?.desc}</p></div>) } */}
                     
+                    {t.transfers.walletBlank? (
+                                                 <p id="blankSec">ENTRY INFO</p>
+
+                    ): (
+                      <p id="walletTitle">ENTRY INFO</p>
+
+                    )}
+
+                    {t.transfers.gate ? (
+                      <p id="walletInfo">{t.transfers?.gate}</p>
+                    ):(
+                      <p id="walletInfo">{t.transfers?.desc}</p>
+                    )  }
                   </li>
                 </ul>
-                <div className="absolute bottom-0 right-0 mb-2">
+
+                {t.transfers.walletBlank?(
+                  <div className="absolute bottom-0  w-full mb-2">
+                  {/* <img src={volumebg} className="w-10" alt="Volume Background" /> */}
+                  <div className='flex  justify-between items-center mx-3 '>
+                    <p >                           
+                             <img src={logoImg} className="w-6" alt="Volume Background" />
+
+
+                    </p>
+                    <p>      
+                                  <img src={volumebg} className="w-10" alt="Volume Background" />
+                    </p>
+                  </div>
+                </div>
+                ):(
+                  <div className="absolute bottom-0 right-0 mb-2">
                   <img src={volumebg} className="w-10" alt="Volume Background" />
                 </div>
+                )}
+                {/* <div className="absolute bottom-0 right-0 mb-2">
+                  <img src={volumebg} className="w-10" alt="Volume Background" />
+                </div> */}
               </div>
             </div>
           ))}
@@ -185,7 +329,7 @@ const Wallet = ({ setIsWalletOpen, eventIndex, seatIndex, handleTicketbtn }) => 
 
       {/* Bottom Section */}
       <div id="wallet-b" className="bg-azure-white" style={{ height: '42vh' }}>
-        <div>
+        <div className='p-5'>
           <IndexBtn
             showWalletBtn={true} // or false depending on the condition
             eventIndex={eventIndex}
