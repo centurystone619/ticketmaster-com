6d9dc8e4485045099a6e248aec5364227019c85d
centurystone619 <michellefoust6@gmail.com>
2025-05-15 12:54:44 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,7 +1,81 @@
 export const eventData=[
   [
+
+        {
+        artiste: "2025 NCAA Men's Basketball Championship - Final Four - Final",
+        img: "https://media.ticketmaster.com/en-us/dam/a/b80/6c99ea06-da08-4213-97d1-49f351fd9b80_1512001_CUSTOM.jpg",
+        sec: "143",
+        row: "GG",
+        seat: "7",
+        date: "Mon, Apr 7",
+        time: "7:50pm",
+        state: "Texas",
+        venue: "Alamodome",
+        type: "Resale ticket",
+        eventData: {
+          month: "Apr",
+          day: "7",
+          year: "2025",
+          time: "Mon - 7:50",
+          region: "",
+          orderNo: ""
+        },
+        transfers: {
+          sec: "",
+          row: "",
+          seats: ['7','8'],
+          desc: "ANY ENTRANCE",
+          GA: "",
+          color: "",
+          venueMap: "https://www.bing.com/maps/geoplat/REST/v1/Imagery/Map/RoadVibrant/29.416967,-98.478813/16?ms=1182,334&ml=Basemap,LandCover,Landmarks,OsmBuildings&key=AnTcaqBi2ypp0xI-OZNi4W_ik2KhjgpqioTAtXLC8GzkMBQRMlyxvxyTnd5b73im&c=en-US&dpi=Large&fmt=jpeg&od=1&shading=hill&logo=n&da=ro&pp=29.416967,-98.478813;dfp;alamodome",
+          gate: "",
+          btnColor: "",
+          sell: "y",
+          trnsfer: "y",
+            addOns:"y",
+            walletBlank:"y",
+        }
+      },
       {
-        artiste: "Teddy Swims - I've Tried Everything But Therapy Tour",
+        artiste: "2025 NCAA Men's Basketball Championship - Final Four - Final",
+        img: "https://media.ticketmaster.com/en-us/dam/a/b80/6c99ea06-da08-4213-97d1-49f351fd9b80_1512001_CUSTOM.jpg",
+        sec: "144",
+        row: "GG",
+        seat: "8",
+        date: "Mon, Apr 7",
+        time: "7:50pm",
+        state: "Texas",
+        venue: "Alamodome",
+        type: "Resale ticket",
+        eventData: {
+          month: "Apr",
+          day: "7",
+          year: "2025",
+          time: "Sat - 7:50",
+          region: "",
+          orderNo: ""
+        },
+        transfers: {
+          sec: "",
+          row: "",
+          seats: ['7','8'],
+          desc: "Resale ticket",
+          GA: "",
+          color: "",
+          venueMap: "https://www.bing.com/maps/geoplat/REST/v1/Imagery/Map/RoadVibrant/29.416967,-98.478813/16?ms=1182,334&ml=Basemap,LandCover,Landmarks,OsmBuildings&key=AnTcaqBi2ypp0xI-OZNi4W_ik2KhjgpqioTAtXLC8GzkMBQRMlyxvxyTnd5b73im&c=en-US&dpi=Large&fmt=jpeg&od=1&shading=hill&logo=n&da=ro&pp=29.416967,-98.478813;dfp;alamodome",
+          gate: "",
+          btnColor: "",
+          sell: "y",
+          trnsfer: "y",
+            addOns:"y",
+             walletBlank:"y",
+        }
+      },
+  ],
+  [
+     
+       {
+           artiste: "Teddy Swims - I've Tried Everything But Therapy Tour",
         img: "https://media.ticketmaster.com/en-us/dam/a/87e/6779f1f2-1911-41b8-aea5-6e8de4c6987e_CUSTOM.jpg",
         sec: "103",
         row: "10",
@@ -25,14 +99,15 @@ export const eventData=[
           seats: ['15','16','17','18'],
           desc: "RAINSHINE*NOROOF",
           GA: "",
-          color: "Black",
-          "venueMap": "https://www.bing.com/maps/geoplat/REST/v1/Imagery/Map/RoadVibrant/34.248585,-77.950554/16?ms=768,240&ml=Basemap,LandCover,Landmarks&key=AnTcaqBi2ypp0xI-OZNi4W_ik2KhjgpqioTAtXLC8GzkMBQRMlyxvxyTnd5b73im&c=en-US&dpi=Large&fmt=jpeg&od=2&shading=hill&logo=n&da=ro&pp=34.248585,-77.950554;dfp.ss;Live+Oak+Bank+Pavilion",
-          "gate": "",
-          "btnColor": "Black",
+          color: "",
+          venueMap: "https://tixel.com/maps/34.248402,-77.950996",
+          gate: "",
+          btnColor: "",
           sell: "y",
-          trnsfer: "y"
+          trnsfer: "y",
+            addOns:"y"
         }
-      },
+      }, 
       {
         artiste: "Teddy Swims - I've Tried Everything But Therapy Tour",
         img: "https://media.ticketmaster.com/en-us/dam/a/87e/6779f1f2-1911-41b8-aea5-6e8de4c6987e_CUSTOM.jpg",
@@ -59,78 +134,82 @@ export const eventData=[
           desc: "RAINSHINE*NOROOF",
           GA: "",
           color: "Black",
-          "venueMap": "https://www.bing.com/maps/geoplat/REST/v1/Imagery/Map/RoadVibrant/34.248585,-77.950554/16?ms=768,240&ml=Basemap,LandCover,Landmarks&key=AnTcaqBi2ypp0xI-OZNi4W_ik2KhjgpqioTAtXLC8GzkMBQRMlyxvxyTnd5b73im&c=en-US&dpi=Large&fmt=jpeg&od=2&shading=hill&logo=n&da=ro&pp=34.248585,-77.950554;dfp.ss;Live+Oak+Bank+Pavilion",
-          "gate": "",
-          "btnColor": "Black",
+          venueMap: "Live Oak Bank Pavilion",
+          gate: "",
+          btnColor: "Black",
           sell: "y",
-          trnsfer: "y"
+          trnsfer: "y",
+            addOns:"y"
         }
       },
-       {
-        artiste: "Teddy Swims - I've Tried Everything But Therapy Tour",
-        img: "https://media.ticketmaster.com/en-us/dam/a/87e/6779f1f2-1911-41b8-aea5-6e8de4c6987e_CUSTOM.jpg",
-        sec: "103",
-        row: "10",
-        seat: "17",
-        date: "Wed, May 14",
-        time: "8pm",
-        state: "North Carolina",
-        venue: "Live Oak Bank Pavilion",
-        type: "Free transfer",
-        eventData: {
-          month: "May",
-          day: "14",
-          year: "",
-          time: "Wed - 8",
-          region: "",
-          orderNo: ""
+      {
+          artiste: "Teddy Swims - I've Tried Everything But Therapy Tour",
+          img: "https://media.ticketmaster.com/en-us/dam/a/87e/6779f1f2-1911-41b8-aea5-6e8de4c6987e_CUSTOM.jpg",
+          sec: "103",
+          row: "10",
+          seat: "17",
+          date: "Wed, May 14",
+          time: "8pm",
+          state: "North Carolina",
+          venue: "Live Oak Bank Pavilion",
+          type: "Free transfer",
+          eventData: {
+            month: "May",
+            day: "14",
+            year: "",
+            time: "Wed - 8",
+            region: "",
+            orderNo: ""
+          },
+          transfers: {
+            sec: "",
+            row: "",
+            seats: ['15','16','17','18'],
+            desc: "RAINSHINE*NOROOF",
+            GA: "",
+            color: "Black",
+            venueMap: "Live Oak Bank Pavilion",
+            gate: "",
+            btnColor: "Black",
+            sell: "y",
+            trnsfer: "y",
+              addOns:"y"
+          }
         },
-        transfers: {
-          sec: "",
-          row: "",
-          seats: ['15','16','17','18'],
-          desc: "RAINSHINE*NOROOF",
-          GA: "",
-          color: "Black",
-          "venueMap": "Live Oak Bank Pavilion",
-          "gate": "",
-          "btnColor": "Black",
-          sell: "y",
-          trnsfer: "y"
-        }
-      }, {
-        artiste: "Teddy Swims - I've Tried Everything But Therapy Tour",
-        img: "https://media.ticketmaster.com/en-us/dam/a/87e/6779f1f2-1911-41b8-aea5-6e8de4c6987e_CUSTOM.jpg",
-        sec: "103",
-        row: "10",
-        seat: "18",
-        date: "Wed, May 14",
-        time: "8pm",
-        state: "North Carolina",
-        venue: "Live Oak Bank Pavilion",
-        type: "Free transfer",
-        eventData: {
-          month: "May",
-          day: "14",
-          year: "",
-          time: "Wed - 8",
-          region: "",
-          orderNo: ""
+        {
+          artiste: "Teddy Swims - I've Tried Everything But Therapy Tour",
+          img: "https://media.ticketmaster.com/en-us/dam/a/87e/6779f1f2-1911-41b8-aea5-6e8de4c6987e_CUSTOM.jpg",
+          sec: "103",
+          row: "10",
+          seat: "18",
+          date: "Wed, May 14",
+          time: "8pm",
+          state: "North Carolina",
+          venue: "Live Oak Bank Pavilion",
+          type: "Free transfer",
+          eventData: {
+            month: "May",
+            day: "14",
+            year: "",
+            time: "Wed - 8",
+            region: "",
+            orderNo: ""
+          },
+          transfers: {
+            sec: "",
+            row: "",
+            seats: ['15','16','17','18'],
+            desc: "RAINSHINE*NOROOF",
+            GA: "",
+            color: "Black",
+            venueMap: "Live Oak Bank Pavilion",
+            gate: "",
+            btnColor: "Black",
+            sell: "y",
+            trnsfer: "y",
+              addOns:"y"
+          }
         },
-        transfers: {
-          sec: "",
-          row: "",
-          seats: ['15','16','17','18'],
-          desc: "RAINSHINE*NOROOF",
-          GA: "",
-          color: "Black",
-          "venueMap": "Live Oak Bank Pavilion",
-          "gate": "",
-          "btnColor": "Black",
-          sell: "y",
-          trnsfer: "y"
-        }
-      },
     ],
   [
     {
@@ -159,11 +238,12 @@ export const eventData=[
         desc: "RAINSHINE*NOROOF",
         GA: "",
         color: "Black",
-        venueMap: "",
+        venueMap: "https://tixel.com/maps/34.248402,-77.950996",
         gate: "",
         btnColor: "Black",
         sell: "y",
-        trnsfer: "y"
+        trnsfer: "y",
+          addOns:"y"
       }
     },
     {
@@ -192,11 +272,12 @@ export const eventData=[
         desc: "RAINSHINE*NOROOF",
         GA: "",
         color: "Black",
-        venueMap: "",
+        venueMap: "https://tixel.com/maps/34.248402,-77.950996",
         gate: "",
         btnColor: "Black",
         sell: "y",
-        trnsfer: "y"
+        trnsfer: "y",
+          addOns:"y"
       }
     },
     {
@@ -224,12 +305,13 @@ export const eventData=[
         seats: ['15','16','17','18'],
         desc: "RAINSHINE*NOROOF",
         GA: "",
-        color: "",
+        color: "Black",
         venueMap: "",
         gate: "",
-        btnColor: "",
+        btnColor: "Black",
         sell: "y",
-        trnsfer: "y"
+        trnsfer: "y",
+          addOns:"y"
       }
     },
     {
@@ -257,12 +339,13 @@ export const eventData=[
         seats: ['15','16','17','18'],
         desc: "RAINSHINE*NOROOF",
         GA: "",
-        color: "",
+        color: "Black",
         venueMap: "",
         gate: "",
-        btnColor: "",
+        btnColor: "Black",
         sell: "y",
-        trnsfer: "y"
+        trnsfer: "y",
+          addOns:"y"
       }
     },
    
