9d681358466387e408d7ce656398aaec5cd39041
centurystone619 <michellefoust6@gmail.com>
2025-07-18 02:10:44 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -146,5 +146,44 @@ export const eventData=[
         }
       },
   ],
+
+  [
+     {
+        artiste: "The Wood Brothers",
+        img: "https://s1.ticketm.net/dam/a/38e/0647dc2e-2f24-4e55-83a0-6233dcf2838e_EVENT_DETAIL_PAGE_16_9.jpg",
+        sec: "GA3",
+        row: "",
+        seat: "",
+        date: "Thu, Aug 14, 2025",
+        time: "6:00 PM",
+        state: "Wilmington, NC",
+        venue: "Greenfield Lake Amphitheater",
+        type: "Standard Admission",
+        eventData: {
+          month: "Jul",
+          day: "19",
+          year: "2025",
+          time: "19:00",
+          region: "",
+          orderNo: ""
+        },
+        transfers: {
+          sec: "",
+          row: "",
+          seats:['','', '',''],
+          ticketId:['1','2'],
+          desc: "GENERAL ADMISSN",
+          GA: "General Admission",
+          color: "",
+          venueMap: "https://www.bing.com/maps/geoplat/REST/v1/Imagery/Map/RoadVibrant/34.210197,-77.939789/16?ms=768,240&sv=9.38&ml=Basemap,LandCover,Landmarks&key=AnTcaqBi2ypp0xI-OZNi4W_ik2KhjgpqioTAtXLC8GzkMBQRMlyxvxyTnd5b73im&c=en-US&dpi=Large&fmt=jpeg&od=2&shading=hill&logo=n&da=ro&pp=34.210197,-77.939789;dfp.ss;Greenfield+Lake+Amphit...",
+          gate: "",
+          btnColor: "",
+          sell: "y",
+          trnsfer: "y",
+            addOns:"y",
+            
+        }
+      },
+  ],
   
 ]
