79bc4b382ae11f1efe1748dcd0bb033389cf0ce4
centurystone619 <michellefoust6@gmail.com>
2025-07-24 20:38:28 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -23,7 +23,7 @@ export const eventData=[
         transfers: {
           sec: "",
           row: "",
-          seats: [“”,“”,“”]
+          seats: [“”,“”,“”],
           ticketId:[“1”,“2”,“3”],
           desc: "JEFFGORDONLWN",
           GA: "General Admission",
