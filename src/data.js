612d8c8269ed223b352d0f441b7788bcfd6d8ba6
centurystone619 <michellefoust6@gmail.com>
2025-07-24 20:21:36 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -71,6 +71,40 @@ export const eventData=[
              walletBlank:"y",
         }
       },
+      {
+        artiste: "The Red Clay Strays: Get Right Tour",
+        img: "https://github.com/centurystone619/Img/blob/main/opp0.jpg?raw=true",
+        sec: "LAWN4",
+        row: "",
+        seat: "",
+        date: "Fri, Aug 15, 2025",
+        time: "7:00 PM",
+        state: "North Carolina",
+        venue: "Live Oak Bank Pavilion",
+        type: "Standard Admission",
+        eventData: {
+          month: "Aug",
+          day: "15",
+          year: "2025",
+          time: "Fri - 7:00",
+          region: "",
+          orderNo: ""
+        },
+        transfers: {
+          sec: "",
+          row: "",
+          seats: ['',''],
+          desc: "JEFFGORDONLWN",
+          GA: "General Admission",
+          color: "black",
+          venueMap: "https://www.bing.com/maps/geoplat/REST/v1/Imagery/Map/RoadVibrant/34.248489,-77.950951/15?ms=312,200&sv=9.41&ml=Basemap,LandCover,Landmarks&key=AnTcaqBi2ypp0xI-OZNi4W_ik2KhjgpqioTAtXLC8GzkMBQRMlyxvxyTnd5b73im&c=en-US&fmt=jpeg&od=2&shading=hill&logo=n&da=ro&pp=34.248489,-77.950951;dfp.ss;Live+Oak+Bank+Pavilion",
+          gate: "",
+          btnColor: "black",
+          sell: "y",
+          trnsfer: "y",
+            addOns:"y",
+            walletBlank:"y",
+        }
   ],
   
 ]
