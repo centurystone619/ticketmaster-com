9998e9ff64cb1e38856a5fe7c52ecb85e8351e4e
centurystone619 <michellefoust6@gmail.com>
2025-07-25 23:19:25 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -115,7 +115,7 @@ export const eventData=[
         sec: "UN",
         row: "J",
         seat: "14",
-        date: "Sat, Aug 30, ",
+        date: "Sat, Aug 30",
         time: "7:30pm",
         state: "South Carolina",
         venue: "Clemson Memorial Stadium",
