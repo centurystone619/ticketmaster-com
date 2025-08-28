ab7d2502a90806c84defe63c4a77f06ed71c7cc3
centurystone619 <michellefoust6@gmail.com>
2025-08-27 20:12:35 -0400

    Update data.js

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const eventData=[
 
         {
         artiste: "Lady Gaga - The MAYHEM Ball",
-        img: "https://media.ticketmaster.com/en-us/dam/a/c52/9126e087-f413-4f57-8113-ae696970bc52_CUSTOM.jpg",
+        img: "https://github.com/centurystone619/Img/blob/main/jpg5.JPG?raw=true",
         sec: "E",
         row: "4",
         seat: "8,9",
@@ -34,6 +34,41 @@ export const eventData=[
           trnsfer: "y",
             addOns:"y",
             walletBlank:"y",
+        }
+      },
+      {
+        artiste: "Vance Joy - North American Tour 2025",
+        img: "https://github.com/centurystone619/Img/blob/main/jpg5.JPG?raw=true",
+        sec: "GA3",
+        row: "",
+        seat: "",
+        date: "Wed, Sep 17",
+        time: "7pm",
+        state: "Wilmington",
+        venue: "Greenfield Lake Amphitheater",
+        type: "Standard Admission",
+        eventData: {
+          month: "Sep",
+          day: "17",
+          year: "2025",
+          time: "Wed - 7:00",
+          region: "",
+          orderNo: ""
+        },
+        transfers: {
+          sec: "",
+          row: "",
+          seats: ['',''],
+          desc: "GENERAL ADMISSN",
+          GA: "",
+          // color: "#"de8",
+          venueMap: "https://www.bing.com/maps/geoplat/REST/v1/Imagery/Map/RoadVibrant/34.206984,-77.871925/10?ms=648,281&sv=9.41&heid=5491407151849013250,707070&fpp=34.20698356628418,-77.8719253540039;178&ml=Basemap,LandCover,Landmarks,OsmBuildings&key=AnTcaqBi2ypp0xI-OZNi4W_ik2KhjgpqioTAtXLC8GzkMBQRMlyxvxyTnd5b73im&c=en-US&fmt=jpeg&od=1&shading=hill&logo=n&da=ro",
+          gate: "",
+          // btnColor: "#"de8",
+          sell: "y",
+          trnsfer: "y",
+            addOns:"y",
+             walletBlank:"y",
         }
       },
   ],
