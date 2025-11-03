a7696b61fffc42512e1a395fe5644a581fbd7b61
centurystone619 <michellefoust6@gmail.com>
2025-11-03 22:24:43 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -17,7 +17,7 @@ export const eventData=[
           month: "Nov",
           day: "01",
           year: "",
-          time: "Sat - 7:30",
+         // time: "Sat - 7:30",
           region: "",
           orderNo: ""
         },
@@ -35,7 +35,9 @@ export const eventData=[
             addOns:"",
             walletColor:'green',
           sportSec:'y',
-          ticketAction:'m'
+         // ticketAction:'m',
+                        footerImg:'https://github.com/centurystone619/Img/blob/main/Vol%20footer%20img.jpeg?raw=true',
+
         }
       },
       {
@@ -113,3 +115,126 @@ export const eventData=[
   ],
   
 ]
+
+
+
+
+
+
+
+
+// export const eventData=[
+// [
+
+// {
+//                artiste: "Vol Football vs. Oklahoma",
+
+//         img: "https://github.com/centurystone619/Img/blob/main/Ojpg.PNG?raw=true",
+//         sec: "X5",
+//         row: "41",
+//         seat: "13",
+//         date: "Sat, Nov 01, 2025",
+//         time: "7:30 PM",
+//         state: "Knoxville, TN",
+//         venue: "Neyland Stadium",
+//         type: "",
+//         eventData: {
+//           month: "Nov",
+//           day: "01",
+//           year: "",
+//           time: "Sat - 7:30",
+//           region: "",
+//           orderNo: ""
+//         },
+//         transfers: {
+//           sec: "",
+//           row: "",
+//           seats: ['14','15','16'],
+//           desc: "NonPremium Gates",
+//           GA: "",
+//           color: "#ff8202",
+//           gate: "",
+//           btnColor: "#ff8202",
+//           sell: "y",
+//           trnsfer: "y",
+//             addOns:"",
+//             walletColor:'green',
+//           sportSec:'y',
+//           ticketAction:'m'
+//         }
+//       },
+//       {
+//         artiste: "Vol Football V Oklahoma",
+//         img: "https://github.com/centurystone619/Img/blob/main/Ojpg.PNG?raw=true",
+//         sec: "X5",
+//         row: "41",
+//         seat: "14",
+//         date: "Sat, Nov 01, 2025",
+//         time: "7:30 PM",
+//         state: "Tennessee",
+//         venue: "Neyland Stadium",
+//         type: "",
+//         eventData: {
+//           month: "Nov",
+//           day: "01",
+//           year: "",
+//           time: "Sat - 7:30",
+//           region: "",
+//           orderNo: ""
+//         },
+//         transfers: {
+//           sec: "",
+//           row: "",
+//           seats: ['',''],
+//           desc: "NonPremium Gates",
+//           GA: "",
+//           color: "#ff8202",
+//           venueMap: "",
+//           gate: "",
+//           btnColor: "#ff8202",
+//           sell: "y",
+//           trnsfer: "y",
+//             addOns:"y",
+//            sportSec:'y',
+//         }
+//       },
+
+//       {
+//         artiste: "Vol Football V Oklahoma",
+//         img: "https://github.com/centurystone619/Img/blob/main/Ojpg.PNG?raw=true",
+//         sec: "X5",
+//         row: "41",
+//         seat: "15",
+//         date: "Sat, Nov 01, 2025",
+//         time: "7:30 PM",
+//         state: "South Carolina",
+//         venue: "Neyland Stadium",
+//         type: "",
+//         eventData: {
+//           month: "Nov",
+//           day: "01",
+//           year: "",
+//           time: "Sat - 7:30",
+//           region: "",
+//           orderNo: ""
+//         },
+//         transfers: {
+//           sec: "",
+//           row: "",
+//           seats: ['',''],
+//           desc: "NonPremium Gates",
+//           GA: "",
+//           color: "#ff8202",
+//           venueMap: "",
+//           gate: "",
+//           btnColor: "#ff8202",
+//           sell: "y",
+//           trnsfer: "y",
+//             addOns:"y",
+//            sportSec:'y',
+//         }
+//         },
+        
+//   ],
+  
+// ]
